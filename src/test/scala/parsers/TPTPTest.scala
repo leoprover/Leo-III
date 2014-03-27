6a80e638b5bfbbaf317687fcfcdc32c8d23177a7
Alex <a.steen@fu-berlin.de>
2014-03-27 12:38:01 +0100

    added test for fof parser

--- src/test/scala/parsers/TPTPTest.scala
+++ src/test/scala/parsers/TPTPTest.scala
@@ -5,12 +5,16 @@ package parsers
  */
 object TPTPTest extends TPTPParser {
   def main(args: Array[String]) {
+    // CNF
     println("#### Test on includeTest ####")
     runTestOn(includeTest)
     println("#### Test on col065_1 ####")
     runTestOn(col065_1)
     println("#### Test on col074_2 ####")
     runTestOn(col074_2)
+    // FOF
+    println("#### Test on mgt011p1 ####")
+    runTestOn(mgt011p1)
   }
 
   def runTestOn(input: String) {
@@ -84,5 +88,70 @@ object TPTPTest extends TPTPParser {
       |
       |cnf(diagonal_combinator,axiom,
       |    ( apply(apply(f,X),Y) = apply(X,X) )).
+    """.stripMargin
+
+
+  // FOF
+  val mgt011p1 : String =
+    """
+      |fof(mp5,axiom,
+      |    ( ! [X,T] :
+      |        ( organization(X,T)
+      |       => ? [I] : inertia(X,I,T) ) )).
+      |
+      |fof(mp6_1,axiom,
+      |    ( ! [X,Y] : ~ ( greater(X,Y)
+      |        & X = Y ) )).
+      |
+      |fof(mp6_2,axiom,
+      |    ( ! [X,Y] : ~ ( greater(X,Y)
+      |        & greater(Y,X) ) )).
+      |
+      |fof(mp9,axiom,
+      |    ( ! [X,T] :
+      |        ( organization(X,T)
+      |       => ? [C] : class(X,C,T) ) )).
+      |
+      |fof(mp10,axiom,
+      |    ( ! [X,T1,T2,C1,C2] :
+      |        ( ( organization(X,T1)
+      |          & organization(X,T2)
+      |          & reorganization_free(X,T1,T2)
+      |          & class(X,C1,T1)
+      |          & class(X,C2,T2) )
+      |       => C1 = C2 ) )).
+      |
+      |fof(a5_FOL,hypothesis,
+      |    ( ! [X,Y,C,S1,S2,I1,I2,T1,T2] :
+      |        ( ( organization(X,T1)
+      |          & organization(Y,T2)
+      |          & class(X,C,T1)
+      |          & class(Y,C,T2)
+      |          & size(X,S1,T1)
+      |          & size(Y,S2,T2)
+      |          & inertia(X,I1,T1)
+      |          & inertia(Y,I2,T2)
+      |          & greater(S2,S1) )
+      |       => greater(I2,I1) ) )).
+      |
+      |fof(t2_FOL,hypothesis,
+      |    ( ! [X,I1,I2,T1,T2] :
+      |        ( ( organization(X,T1)
+      |          & organization(X,T2)
+      |          & reorganization_free(X,T1,T2)
+      |          & inertia(X,I1,T1)
+      |          & inertia(X,I2,T2)
+      |          & greater(T2,T1) )
+      |       => greater(I2,I1) ) )).
+      |
+      |fof(t11_FOL,conjecture,
+      |    ( ! [X,S1,S2,T1,T2] :
+      |        ( ( organization(X,T1)
+      |          & organization(X,T2)
+      |          & reorganization_free(X,T1,T2)
+      |          & size(X,S1,T1)
+      |          & size(X,S2,T2)
+      |          & greater(T2,T1) )
+      |       => ~ greater(S1,S2) ) )).
     """.stripMargin
 }
