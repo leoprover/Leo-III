18aa20f4728a81b9f10a4400a15599afc67a09e7
Alex <a.steen@fu-berlin.de>
2014-04-03 11:50:57 +0200

    added parenthesis for formulae precendence

--- src/main/scala/tptp/fof/Formula.scala
+++ src/main/scala/tptp/fof/Formula.scala
@@ -5,20 +5,36 @@ import tptp.Commons._
  * Created by lex on 3/23/14.
  */
 sealed abstract class Formula
-case class Logical(formula: LogicFormula) extends Formula
-case class Sequent(tuple1: List[LogicFormula], tuple2: List[LogicFormula]) extends Formula
+case class Logical(formula: LogicFormula) extends Formula {
+  override def toString = formula.toString
+}
+case class Sequent(tuple1: List[LogicFormula], tuple2: List[LogicFormula]) extends Formula {
+  override def toString = "[" + tuple1.mkString(",") +"]" + " --> " + "[" + tuple2.mkString(",") + "]"
+}
 
 sealed abstract class LogicFormula
-case class Binary(left: LogicFormula, connective: BinaryConnective, right: LogicFormula) extends LogicFormula
-case class Unary(connective: UnaryConnective, formula: LogicFormula) extends LogicFormula
-case class Quantified(quantifier: Quantifier, varList: List[Variable], matrix: LogicFormula) extends LogicFormula
-case class Atomic(formula: AtomicFormula) extends LogicFormula
-case class Inequality(left: Term, right: Term) extends LogicFormula
+case class Binary(left: LogicFormula, connective: BinaryConnective, right: LogicFormula) extends LogicFormula  {
+  override def toString = "(" + left.toString + ") " + connective.toString + " (" + right.toString + ")"
+}
+case class Unary(connective: UnaryConnective, formula: LogicFormula) extends LogicFormula {
+  override def toString = connective.toString + " (" + formula.toString + ")"
+}
+case class Quantified(quantifier: Quantifier, varList: List[Variable], matrix: LogicFormula) extends LogicFormula {
+  override def toString = quantifier.toString + " [" + varList.mkString(",") + "] : (" + matrix.toString + ")"
+}
+case class Atomic(formula: AtomicFormula) extends LogicFormula {
+  override def toString = formula.toString
+}
+case class Inequality(left: Term, right: Term) extends LogicFormula {
+  override def toString = left.toString + " != " + right.toString
+}
 
 
 sealed abstract class BinaryConnective
 case object <=> extends BinaryConnective
-case object Impl extends BinaryConnective
+case object Impl extends BinaryConnective {
+  override def toString = "=>"
+}
 case object <= extends BinaryConnective
 case object <~> extends BinaryConnective
 case object ~| extends BinaryConnective
@@ -27,9 +43,11 @@ case object | extends BinaryConnective
 case object & extends BinaryConnective
 
 sealed abstract class UnaryConnective
-case object Not extends UnaryConnective
+case object Not extends UnaryConnective  {
+  override def toString = "~"
+}
 
 sealed abstract class Quantifier
-case object Forall extends Quantifier
-case object Exists extends Quantifier
+case object ! extends Quantifier
+case object ? extends Quantifier
 
