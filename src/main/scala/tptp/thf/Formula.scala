54f0e025d2ea2490c781581d929b9a41217c4001
Alex <a.steen@fu-berlin.de>
2014-04-03 12:24:46 +0200

    added thf tostring instances

--- src/main/scala/tptp/thf/Formula.scala
+++ src/main/scala/tptp/thf/Formula.scala
@@ -14,15 +14,31 @@ case class Sequent(tuple1: List[LogicFormula], tuple2: List[LogicFormula]) exten
 }
 
 sealed abstract class LogicFormula
-case class Typed(formula: LogicFormula, typ: LogicFormula) extends LogicFormula
-case class Binary(left: LogicFormula, connective: BinaryConnective, right: LogicFormula) extends LogicFormula
-case class Unary(connective: UnaryConnective, formula: LogicFormula) extends LogicFormula
-case class Quantified(quantifier: Quantifier, varList: List[(Commons.Variable,Option[LogicFormula])], matrix: LogicFormula) extends LogicFormula
+case class Typed(formula: LogicFormula, typ: LogicFormula) extends LogicFormula {
+  override def toString = formula.toString + " : " + typ.toString
+}
+case class Binary(left: LogicFormula, connective: BinaryConnective, right: LogicFormula) extends LogicFormula {
+  override def toString = "(" + left.toString + ") " + connective.toString + " (" + right.toString + ")"
+}
+case class Unary(connective: UnaryConnective, formula: LogicFormula) extends LogicFormula {
+  override def toString = connective.toString + " (" + formula.toString + ")"
+}
+case class Quantified(quantifier: Quantifier, varList: List[(Commons.Variable,Option[LogicFormula])], matrix: LogicFormula) extends LogicFormula {
+  override def toString = quantifier.toString + " [" + varList.mkString(",") + "] : (" + matrix.toString + ")"
+}
 case class Connective(c: Either[BinaryConnective, UnaryConnective]) extends LogicFormula
-case class Term(t: Commons.Term) extends LogicFormula
-case class BinType(t: BinaryType) extends LogicFormula
-case class Subtype(left: String, right: String) extends LogicFormula
-case class Cond(cond: LogicFormula, thn: LogicFormula, els: LogicFormula) extends LogicFormula
+case class Term(t: Commons.Term) extends LogicFormula {
+  override def toString = t.toString
+}
+case class BinType(t: BinaryType) extends LogicFormula {
+  override def toString = t.toString
+}
+case class Subtype(left: String, right: String) extends LogicFormula {
+  override def toString = left + " << " + right
+}
+case class Cond(cond: LogicFormula, thn: LogicFormula, els: LogicFormula) extends LogicFormula{
+  override def toString = "$ite_f(" + List(cond,thn,els).mkString(",") + ")"
+}
 // Let omitted
 
 sealed abstract class BinaryConnective
@@ -62,7 +78,13 @@ case object @- extends Quantifier // Description
 
 // type TopType = LogicFormula
 sealed abstract class BinaryType
-case class ->(t: List[LogicFormula]) extends BinaryType
-case class *(t: List[LogicFormula]) extends BinaryType
-case class +(t: List[LogicFormula]) extends BinaryType
+case class ->(t: List[LogicFormula]) extends BinaryType {
+  override def toString = "(" + t.mkString(" > ") + ")"
+}
+case class *(t: List[LogicFormula]) extends BinaryType {
+  override def toString = "(" + t.mkString(" * ") + ")"
+}
+case class +(t: List[LogicFormula]) extends BinaryType {
+  override def toString = "(" + t.mkString(" + ") + ")"
+}
 
