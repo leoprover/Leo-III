53159a8302a7c217ddc4f7399d1570dfe1c237da
Alex <a.steen@fu-berlin.de>
2014-05-02 15:30:37 +0200

    implemented signature for first test

--- src/main/scala/datastructures/internal/Signature.scala
+++ src/main/scala/datastructures/internal/Signature.scala
@@ -1,43 +1,151 @@
 package datastructures.internal
 
-import scala.collection.immutable.{IntMap, HashMap}
+import scala.collection.immutable.{HashSet, BitSet, IntMap, HashMap}
 
 /**
  * Created by lex on 29.04.14.
  */
-class Signature extends IsSignature with HOLSignature {
+abstract sealed class Signature extends IsSignature with HOLSignature {
+  override type ConstKey = Int // Invariant: Positive integers
+  override type VarKey = Int // Invariant: Negative integers
+  var curConstKey = 1
+  var curVarKey = -1
+
   protected var keyMap: Map[String, Int] = new HashMap[String, Int]
   protected var metaMap: IntMap[Meta[Int]] = IntMap.empty
 
-  override type ConstKey = Int
-  override type VarKey = Int
+  protected var typeVarsSet, termVarsSet: BitSet = BitSet.empty
+  protected var typeSet, fixedSet, definedSet, uiSet: BitSet = BitSet.empty
 
-  def getUninterpretedSymbols: Set[ConstKey] = ???
+  protected def addConstant0(identifier: String, typ: Option[Type], defn: Option[Term]): ConstKey = {
+    val key = curConstKey
+    curConstKey += 1
+    keyMap += ((identifier, key))
 
-  def getFixedDefinedSymbols: Set[ConstKey] = ???
+    defn match {
+      case None => { // Uninterpreted or type
+        typ match {
+          case None => assert(false, "This should not happen")
+          case Some(k:Kind) => { // Type
+            true match {
+              case k.isBaseKind => {
+                val meta = TypeMeta(identifier, key, k, Type.mkBaseType(identifier))
+                metaMap += (key, meta)
+              }
+              case k.isFunKind => {
+                val meta = TypeMeta(identifier, key, k, Type.mkConstructorType(identifier))
+                metaMap += (key, meta)
+              }
+              case true => {
+                val meta = TypeMeta(identifier, key, Type.getSuperKind, Type.getSuperKind)
+                metaMap += (key, meta)
+              }
+            }
+            typeSet += key
+          }
+          case Some(t:Type) => { // Uninterpreted symbol
+            val meta = UninterpretedMeta(identifier, key, t)
+            metaMap += (key, meta)
+            uiSet += key
+          }
+        }
+      }
 
-  def getDefinedSymbols: Set[ConstKey] = ???
+      case Some(fed) => { // Defined
+        val meta = DefinedMeta(identifier, key, typ, fed)
+        metaMap += (key, meta)
+        definedSet += key
+      }
+    }
 
-  def getBaseTypes: Set[ConstKey] = ???
+    key
+  }
 
-  def getAllConstants: Set[ConstKey] = ???
 
-  def getTypeVariables: Set[VarKey] = ???
 
-  def getTermVariables: Set[VarKey] = ???
+  protected def addVariable0(identifier: String, typ: Option[Type]): VarKey = {
+    val key = curVarKey
+    curVarKey -= 1
+    keyMap += ((identifier, key))
+    typ match {
+      case None => {
+        val meta = TermVarMeta(identifier, key, None) // A variable with no type is assumed
+        metaMap += (key, meta)                        // to be a `TermVariable`
+        termVarsSet += -key
+      }
+      case Some(k: Kind) => {
+        val meta = TypeVarMeta(identifier, key, k)
+        metaMap += (key, meta)
+        typeVarsSet += -key
+      }
+      case Some(ty: Type) => {
+        val meta = TermVarMeta(identifier, key, Some(ty))
+        metaMap += (key, meta)
+        termVarsSet += -key
+      }
+    }
+    key
+  }
 
-  def getAllVariables: Set[VarKey] = ???
+  protected def addFixed(identifier: String, typ: Type): Unit = {
+    val key = curConstKey
+    curConstKey += 1
+    keyMap += ((identifier, key))
 
-  def getConstMeta(identifier: String): ConstMeta = ???
+    val meta = FixedMeta(identifier, key, typ)
+    metaMap += (key, meta)
+  }
 
-  def getConstMeta(key: ConstKey): ConstMeta = ???
+  // naive implementation, will change in the future
+  def getUninterpretedSymbols: Set[ConstKey] = uiSet.toSet
 
-  protected def addConstant0(identifier: String, typ: Option[Type], defn: Option[Term]): ConstKey = ???
+  def getFixedDefinedSymbols: Set[ConstKey] = fixedSet.toSet
 
-  def getVarMeta(identifier: String): VarMeta = ???
+  def getDefinedSymbols: Set[ConstKey] = definedSet.toSet
 
-  def getVarMeta(key: VarKey): VarMeta = ???
+  def getBaseTypes: Set[ConstKey] = typeSet.toSet
 
-  protected def addVariable0(identifier: String, typ: Option[Type]): VarKey = ???
+  def getAllConstants: Set[ConstKey] = uiSet | fixedSet | definedSet | typeSet
+
+  def getTypeVariables: Set[VarKey] = asHashSet(typeVarsSet).map(i => -i).toSet
+  def getTermVariables: Set[VarKey] = asHashSet(termVarsSet).map(i => -i).toSet
+
+  def getAllVariables: Set[VarKey] = getTypeVariables | getTermVariables
+
+
+  protected def asHashSet(set: Set[Int]) = {
+    val e: HashSet[Int] = HashSet.empty
+    set.foldLeft(e)(_+_)
+  }
+
+
+  def getConstMeta(identifier: String): ConstMeta = getConstMeta(keyMap(identifier))
+
+  def getConstMeta(key: ConstKey): ConstMeta = metaMap(key).asInstanceOf[ConstMeta]
+
+  def getVarMeta(identifier: String): VarMeta =  getVarMeta(keyMap(identifier))
+
+  def getVarMeta(key: VarKey): VarMeta = metaMap(key).asInstanceOf[VarMeta]
 }
 
+
+object Signature {
+  private case object Nil extends Signature
+
+  def empty: Signature = Signature.Nil
+  def create: Signature = {
+    val sig = empty
+    for ((name, k) <- sig.types) {
+      sig.addConstant0(name, Some(k), None)
+    }
+
+    for ((name, ty) <- sig.fixedConsts) {
+      sig.addFixed(name, ty)
+    }
+
+    for ((name, fed, ty) <- sig.definedConsts) {
+      sig.addConstant0(name, Some(ty), Some(fed))
+    }
+    sig
+  }
+}
--- src/main/scala/datastructures/internal/Type.scala
+++ src/main/scala/datastructures/internal/Type.scala
@@ -16,7 +16,11 @@ object Type {
   def mkTypeVarType = TypeVarType(_)
   def mkTypeVarType(name: String): Type = mkTypeVarType(mkTypeVar(name))
   def mkForAllType = ForallType(_,_)
+
+  def mkConstructorType = ConstructorType(_)
+  def mkConstructorType(name: String): Type = mkConstructorType(TypeConstructor.mkConstructor(name))
   def getBaseKind = BaseKind
+  def getSuperKind = SuperKind
   def mkFunKind = FunKind(_,_)
 
   implicit def strToType(in: String): Type = {
@@ -61,6 +65,10 @@ case class ContructorAppType(typeCon: TypeConstructor, args: List[Type]) extends
   def pretty = typeCon.pretty + " " + args.map(x => x.pretty).mkString(" ")
 }
 
+case class ConstructorType(typeCon: TypeConstructor) extends Type {
+  def pretty = typeCon.pretty
+}
+
 /** Type of a (bound) type variable when itself used as type in polymorphic function */
 case class TypeVarType(typeVar: TypeVar) extends Type {
   def pretty = typeVar.name // name instead of pretty to avoid repitition of types
@@ -72,23 +80,40 @@ case class BaseType(name: String) extends Type {// string???
 }
 /** Abstract type of kinds (i.e. types of types).
   * The `Kind` class extends the `Type` class to allow unified handling */
-sealed abstract class Kind extends Type
+sealed abstract class Kind extends Type {
+  val isBaseKind: Boolean
+  val isFunKind: Boolean
+}
 /** Represents the kind `*` (i.e. the type of a type) */
 case object BaseKind extends Kind {
   val name = "#*"
   def pretty = "*"
+  val isBaseKind = true
+  val isFunKind = false
 }
 /** Constructs function kinds.
   * Invariant: `out` must not be `SuperKind`. */
 case class FunKind(in: Type, body: Kind) extends Kind {
   def pretty = in.pretty + " -> " + body.pretty
+  val isBaseKind = false
+  val isFunKind = true
 }
 
 /** Artificial kind that models the type of `*` (represented by `BaseKind`) */
 case object SuperKind extends Kind {
   def pretty = "**"
+
+  val isBaseKind = false
+  val isFunKind = false
 }
 
 
+object TypeConstructor {
+  def mkConstructor = TypeCon(_)
+}
+
 /** For future use: Type constructor representation */
 sealed abstract class TypeConstructor extends Pretty
+case class TypeCon(name: String) extends TypeConstructor {
+  def pretty = name
+}
