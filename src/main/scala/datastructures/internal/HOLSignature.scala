606d82fb922fff0bee3e746a9bd3b76e30267a64
Alex <a.steen@fu-berlin.de>
2014-05-02 11:30:33 +0200

    added standard hol symbols to hol signature trait

--- src/main/scala/datastructures/internal/HOLSignature.scala
+++ src/main/scala/datastructures/internal/HOLSignature.scala
@@ -7,34 +7,37 @@ import Variable.{mkTypeVar, typeVarNames}
  *  1. Fixed (interpreted) symbols
  *  2. Defined symbols
  *  3. Standard base types
+ *
+ * @author Alexander Steen
+ * @since 02.05.2014
  */
 trait HOLSignature {
   val types = List((Type.o.name,      getBaseKind),
                    (Type.i.name,      getBaseKind),
                    (getBaseKind.name,   SuperKind))
 
-  val fixedConsts = List(("$true",                                            Type.o),
-                         ("$false",                                           Type.o),
-                         ("#box",                                  Type.o ->: Type.o),
-                         ("#diamond",                              Type.o ->: Type.o),
-                         ("~",                                     Type.o ->: Type.o),
-                         ("!",        mkTypeVar("X") #!  ("X" ->: Type.o) ->: Type.o),
-                         ("|",                          Type.o ->: Type.o ->: Type.o),
-                         ("=",              mkTypeVar("X") #! "X" ->: "X" ->: Type.o))
+  val fixedConsts = List(("$true",                                              Type.o),
+                         ("$false",                                             Type.o),
+                         ("#box",                                    Type.o ->: Type.o),
+                         ("#diamond",                                Type.o ->: Type.o),
+                         ("~",                                       Type.o ->: Type.o),
+                         ("!",        mkTypeVar("X") #!  (("X" ->: Type.o) ->: Type.o)),
+                         ("|",                            Type.o ->: Type.o ->: Type.o),
+                         ("=",              mkTypeVar("X") #! ("X" ->: "X" ->: Type.o)))
 
-  val definedConsts = List(("?",   existsDef, mkTypeVar("X") #!  ("X" ->: Type.o) ->: Type.o),
+  val definedConsts = List(("?",   existsDef, mkTypeVar("X") #! (("X" ->: Type.o) ->: Type.o)),
                            ("&",   andDef,                      Type.o ->: Type.o ->: Type.o),
                            ("=>",  implDef,                     Type.o ->: Type.o ->: Type.o),
                            ("<=",  ifDef,                       Type.o ->: Type.o ->: Type.o),
                            ("<=>", iffDef,                      Type.o ->: Type.o ->: Type.o))
 
-  protected def existsDef: Term = ???
+  protected def existsDef: Term = null
 
-  protected def andDef: Term = ???
+  protected def andDef: Term = null
 
-  protected def implDef: Term = ???
+  protected def implDef: Term = null
 
-  protected def ifDef: Term = ???
+  protected def ifDef: Term = null
 
-  protected def iffDef: Term = ???
+  protected def iffDef: Term = null
 }
---
+++ src/test/scala/datastructures/internal/HOLSignatureTest.scala
@@ -0,0 +1,8 @@
+package datastructures.internal
+
+/**
+ * Created by lex on 02.05.14.
+ */
+object HOLSignatureTest {
+
+}
