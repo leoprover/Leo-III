0594c822f2950b7e85424aa4c253a7f3b4f650d4
Alex <a.steen@fu-berlin.de>
2014-05-02 11:31:08 +0200

    a little test for pretty printing standard hol symbols

--- src/test/scala/datastructures/internal/HOLSignatureTest.scala
+++ src/test/scala/datastructures/internal/HOLSignatureTest.scala
@@ -3,6 +3,14 @@ package datastructures.internal
 /**
  * Created by lex on 02.05.14.
  */
-object HOLSignatureTest {
+object HOLSignatureTest extends HOLSignature {
+    def main(args: Array[String]) {
+      definedConsts.map(f).foreach(println(_))
+      println("#######")
+      fixedConsts.map(g).foreach(println(_))
+    }
+
+    def f(in: (String,Term, Type)): (String, String) =(in._1,in._3.pretty)
+    def g(in: (String,Type)): (String, String) =(in._1,in._2.pretty)
 
 }
