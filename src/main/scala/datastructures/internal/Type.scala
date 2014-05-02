91689b614da27d46980965d9226a054b48334d52
Alex <a.steen@fu-berlin.de>
2014-05-02 10:44:14 +0200

    implemented pretty trait for types and variables

--- src/main/scala/datastructures/internal/Type.scala
+++ src/main/scala/datastructures/internal/Type.scala
@@ -1,6 +1,7 @@
 package datastructures.internal
 
 import Variable.{mkTypeVar}
+import datastructures.Pretty
 
 object Type {
   val o = mkBaseType("$o")
@@ -33,45 +34,58 @@ object Type {
  * @author Alexander Steen
  * @since 30.04.2014
  */
-sealed abstract class Type {
+sealed abstract class Type extends Pretty {
   def ->:(body: Type) = FunType(this, body)
 }
 
 /**
  * Type of a polymorphic function
  * @param typeVar The type variable that is introduced to the type in `in`
- * @param in The type that can now use the type variable `typeVar` as bound type variable
+ * @param body The type that can now use the type variable `typeVar` as bound type variable
  */
-case class ForallType(typeVar: TypeVar, in: Type) extends Type
+case class ForallType(typeVar: TypeVar, body: Type) extends Type {
+  def pretty = "forall " + typeVar.pretty + ". " + body.pretty
+}
 
 /**Constructor for a function type `in -> out` */
-case class FunType(in: Type, out: Type) extends Type
+case class FunType(in: Type, out: Type) extends Type {
+  def pretty = in.pretty + " -> " + out.pretty
+}
 
 /** Application of type arguments to a constructor, yielding a Type
  * @deprecated Not yet implemented, but planned for future use */
-case class ContructorAppType(typeCon: TypeConstructor, args: List[Type]) extends Type
+case class ContructorAppType(typeCon: TypeConstructor, args: List[Type]) extends Type {
+  def pretty = typeCon.pretty + " " + args.map(x => x.pretty).mkString(" ")
+}
 
 /** Type of a (bound) type variable when itself used as type in polymorphic function */
-case class TypeVarType(typeVar: TypeVar) extends Type
+case class TypeVarType(typeVar: TypeVar) extends Type {
+  def pretty = typeVar.pretty
+}
 
 /** Literal type, i.e. `$o` */
-case class BaseType(name: String) extends Type // string???
-
+case class BaseType(name: String) extends Type {// string???
+  def pretty = name
+}
 /** Abstract type of kinds (i.e. types of types).
   * The `Kind` class extends the `Type` class to allow unified handling */
 sealed abstract class Kind extends Type
 /** Represents the kind `*` (i.e. the type of a type) */
 case object BaseKind extends Kind {
   val name = "#*"
+  def pretty = "*"
 }
 /** Constructs function kinds.
-  * Invariant: at least one of `in` or `out` must (transitively) contain `TypeKind`,
-  * otherwise it's a `FunType`. */
-case class FunKind(in: Type, out: Type) extends Kind
+  * Invariant: `out` must not be `SuperKind`. */
+case class FunKind(in: Type, body: Kind) extends Kind {
+  def pretty = in.pretty + " -> " + body.pretty
+}
 
 /** Artificial kind that models the type of `*` (represented by `BaseKind`) */
-case object SuperKind extends Kind
+case object SuperKind extends Kind {
+  def pretty = "**"
+}
 
 
 /** For future use: Type constructor representation */
-sealed abstract class TypeConstructor
+sealed abstract class TypeConstructor extends Pretty
--- src/main/scala/datastructures/internal/Variable.scala
+++ src/main/scala/datastructures/internal/Variable.scala
@@ -1,5 +1,7 @@
 package datastructures.internal
 
+import datastructures.Pretty
+
 /**
  *
  *
@@ -14,9 +16,11 @@ object Variable {
   var lastUsedIndex = -1
 }
 
-abstract sealed class Variable {
+abstract sealed class Variable extends Pretty {
   def isTypeVar: Boolean
   def isTermVar: Boolean
+
+  def hasType: Boolean
 
   //vielleicht local/global scope speichern? (Alex)
 }
@@ -24,11 +28,22 @@ case class TypeVar(name: String, varType: Kind) extends Variable {
   def isTypeVar = true
   def isTermVar = false
 
+  def hasType = true
+
   def #!(body: Type): ForallType = {
     ForallType(this, body)
   }
+
+  def pretty = name + ":" + varType.pretty
 }
-case class TermVar(name: String, varType: Type) extends Variable {
+case class TermVar(name: String, varType: Option[Type]) extends Variable {
   def isTypeVar = false
   def isTermVar = true
+
+  def hasType = varType.isDefined
+
+  def pretty = varType match {
+    case None      => name
+    case Some(typ) => name + ":" + typ.pretty
+  }
 }
