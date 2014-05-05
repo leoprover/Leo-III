afccdf68bb8b5653d05874d4108779c7074e0e22
Alex <a.steen@fu-berlin.de>
2014-05-05 21:04:56 +0200

    added test case for HOL signature trait

--- src/test/scala/datastructures/internal/HOLSignatureTest.scala
+++ src/test/scala/datastructures/internal/HOLSignatureTest.scala
@@ -1,16 +1,24 @@
 package datastructures.internal
 
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
 /**
- * Created by lex on 02.05.14.
+ * This test checks if the number of predefined symbols is coherent with the
+ * description in [[datastructures.internal.HOLSignature]].
+ *
+ * @author Alexander Steen
+ * @since 05.05.2014
  */
-object HOLSignatureTest extends HOLSignature {
-    def main(args: Array[String]) {
-      definedConsts.map(f).foreach(println(_))
-      println("#######")
-      fixedConsts.map(g).foreach(println(_))
-    }
-
-    def f(in: (String,Term, Type)): (String, String) =(in._1,in._3.pretty)
-    def g(in: (String,Type)): (String, String) =(in._1,in._2.pretty)
-
+@RunWith(classOf[JUnitRunner])
+class HOLSignatureTest extends FunSuite with HOLSignature {
+  test("cardinality of fixed symbols") {
+    assert(fixedConsts.length == 8)
+  }
+  test("cardinality of defined symbols") {
+    assert(definedConsts.length == 5)
+  }
+  test("cardinality of type symbols") {
+    assert(types.length == 3)
+  }
 }
