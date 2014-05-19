3e1fb6d1bd5b47ed32925299026cfe066875a9c3
Max Wisniewski <max.wisniewski@fu-berlin.de>
2014-05-19 14:26:36 +0200

    Started working on Active Objects for Scheduler

---
+++ src/main/scala/scheduler/Scheduler.scala
@@ -0,0 +1,13 @@
+package scheduler
+
+/**
+ * exi<p>
+ * Central Object for coordinating the ThreadPool responsible for
+ * executing the Agents
+ * </p>
+ * @author Max Wisniewski
+ * @since 5/15/14
+ */
+object Scheduler {
+
+}
