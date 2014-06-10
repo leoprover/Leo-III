6b9eafe5dd28d7fdff9972c7c3428750abe028b1
Alex <a.steen@fu-berlin.de>
2014-06-11 01:59:51 +0200

    added church numerals with successor and add function as module for leo

--- src/main/scala/leo/modules/churchNumerals/Numerals.scala
+++ src/main/scala/leo/modules/churchNumerals/Numerals.scala
@@ -14,9 +14,11 @@ import leo.datastructures.internal.Type.{typeVarToType,∀}
 object Numerals {
   type Church = Term
 
+  /** The zero in church numeral representation */
   def zero: Church = Λ(λ(1 ->: 1,1)(1,1))
 
-  def succ: Church = λ(∀((1 ->: 1) ->: 1 ->: 1))(
+  /** The successor function for church numerals */
+  def succ: Term = λ(∀((1 ->: 1) ->: 1 ->: 1))(
                           Λ(
                             λ(1 ->:1, 1)(
                               ap(
@@ -29,14 +31,41 @@ object Numerals {
                               )
                             )
                           )
+
+  /** applies the successor function `succ` to the given church numeral */
   def succ(n: Church): Church = ap(succ, n).betaNormalize
 
-  def main(args: Array[String]) {
-    println(succ.ty.pretty)
-    println(succ.pretty)
+  /** The add function for church numerals */
+  def add: Term = λ(∀((1 ->: 1) ->: 1 ->: 1),∀((1 ->: 1) ->: 1 ->: 1))(
+                    Λ(
+                      λ(1 ->: 1, 1)(
+                        ap(
+                          ap(
+                            tyAp((4,∀((1 ->: 1) ->: 1 ->: 1)),1),
+                            (2,1 ->: 1)
+                          ),
+                          ap(
+                            ap(
+                              tyAp((3,∀((1 ->: 1) ->: 1 ->: 1)),1),
+                              (2,1 ->: 1)
+                            ),
+                            (1,1)
+                          )
+                        )
+                      )
+                    )
+                  )
 
-    println(zero.pretty)
-    println(ap(succ,zero).pretty)
-    println(ap(succ,zero).betaNormalize.pretty)
+  /** applies the `àdd` function to the given arguments. */
+  def add(n: Church, m: Church): Church = ap(ap(add, n), m).betaNormalize
+
+  implicit def fromInt(n: Int): Church = {
+    require(n >= 0, "Church numerals cannot be negative.")
+    n match {
+      case 0 => zero
+      case m => succ(m-1)
+    }
   }
 }
+
+
