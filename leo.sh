6d3ffcef42e7c78950357d31673da9061dc77ca7
Max Wisniewski <max.wisniewski@fu-berlin.de>
2014-06-24 13:23:50 +0200

    SBT Console workaround

---
+++ leo.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# First compile the project
+sbt compile
+
+# Insert classpath here
+# May get through 'sbt "show fullClasspath"'
+scala -classpath "target/scala-2.10/classes:/home/ryu/.ivy2/cache/org.scala-stm/scala-stm_2.10/jars/scala-stm_2.10-0.7.jar" -i src/main/scala/LeoShell.scala
