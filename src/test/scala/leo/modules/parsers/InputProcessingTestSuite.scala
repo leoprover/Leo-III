93e450be09ae0d4c8c6acbd4a483f743be7a9586
Alex <a.steen@fu-berlin.de>
2014-07-03 12:14:12 +0200

    added test suite for input processing. so far: up to blacklists, every syn problem can be processed

--- src/test/scala/leo/modules/parsers/InputProcessingTestSuite.scala
+++ src/test/scala/leo/modules/parsers/InputProcessingTestSuite.scala
@@ -16,7 +16,21 @@ class InputProcessingTestSuite extends FunSuite {
   val __TPTPPROBLEMPATH__ = "/home/lex/Downloads/TPTP-v6.0.0/Problems"
 
   val files = new File(__TPTPPROBLEMPATH__ +"/SYN").listFiles.filter(_.getName.endsWith(".p")).toSet
-  val blacklist: Set[File] = files.filter(_.getName.contains("-"))
+  val blacklist: Set[File] = (files.filter(_.getName.contains("-")) ++
+                              files.filter({
+                                      case f =>
+                                        Set("SYN000_2.p" // contains product types
+                                          , "SYN000_1.p" // contains product types
+                                          , "SYN357^7.p" // contains imports
+                                          , "SYN367^7.p" // contains imports
+                                          , "SYN036^7.p" // contains imports
+                                          , "SYN052^7.p" // contains imports
+                                          , "SYN377^7.p" // contains imports
+                                          , "SYN407^7.p" // contains imports
+                                          , "SYN397^7.p" // contains imports
+                                          , "SYN000=2.p" // we dont have all tptp defined symbols
+                                          , "SYN000^2.p" // let not yet implemented
+                                        ).contains(f.getName)}))
   val sig = Signature.get
 
   for (f <- files -- blacklist) {
