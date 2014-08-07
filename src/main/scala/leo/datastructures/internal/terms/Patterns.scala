934f6c1b67d61715fcb8eba61e5c242261eefef1
Alex <a.steen@fu-berlin.de>
2014-08-07 18:41:17 +0200

    implemented pattern match unapply objects suitable for spine notation

--- src/main/scala/leo/datastructures/internal/terms/Patterns.scala
+++ src/main/scala/leo/datastructures/internal/terms/Patterns.scala
@@ -1,8 +1,42 @@
 package leo.datastructures.internal.terms
 
-/**
- * Created by lex on 07.08.14.
- */
-class Patterns {
+import leo.datastructures.internal.{HOLBinaryConnective, Signature, Type, Term}
 
+object Bound {
+  def unapply(t: TermImpl): Option[(Type, Int)] = t match {
+    case Root(BoundIndex(t,scope), SNil) => Some((t, scope))
+    case _                               => None
+  }
 }
+
+object Symbol {
+  def unapply(t: TermImpl): Option[Signature#Key] = t match {
+    case Root(UiAtom(id), SNil)  => Some(id)
+    case Root(DefAtom(id), SNil) => Some(id)
+    case _                       => None
+  }
+}
+
+object TermAbstraction {
+  def unapply(t: TermImpl): Option[(Type, Term)] = t match {
+    case TermAbstr(t, body) => Some((t, body))
+    case _                  => None
+  }
+}
+
+object @@@ extends HOLBinaryConnective {
+  import TermImpl.headToTerm
+
+  val key = Integer.MIN_VALUE
+
+  def unapply(t: TermImpl): Option[(Term, Seq[Term])] = t match {
+    case Root(hd, args)    => Some((headToTerm(hd), args.asTerms))
+    case Redex(left, args) => Some((left, args.asTerms))
+    case _                 => None
+  }
+
+//  override def apply(left: TermImpl, right: TermImpl): Term = TermImpl.mkTermApp(left,right)
+}
+
+
+
