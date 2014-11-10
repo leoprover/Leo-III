c22345467c688c1a21303505e91ffed7e3b26b7d
Alex <a.steen@fu-berlin.de>
2014-11-10 18:58:55 +0100

    added first version of logging wrapper trait and Console as an output object writing to system.out

--- src/main/scala/leo/modules/logger/Console.scala
+++
@@ -1,6 +0,0 @@
-//package leo.modules.logger
-//
-//
-//object Console extends Logging {
-//  override def logName = "Console"
-//}
---
+++ src/main/scala/leo/modules/output/logger/Logging.scala
@@ -0,0 +1,44 @@
+package leo.modules.output.logger
+
+import java.util.logging.Logger
+import java.util.logging.Level
+
+import leo.modules.output.Output
+
+/**
+ *
+ * @author Alexander Steen
+ * @since 10.11.2014
+ * @note  Replaces obsolete trait formerly located at leo.modules.logger.Logging.
+ */
+trait Logging {
+  private lazy val log = {val l = Logger.getLogger(loggerName)
+    l.setLevel(defaultLogLevel)
+    l.setUseParentHandlers(useParentLoggers); l}
+
+  protected def loggerName: String = getClass.getName
+  protected def defaultLogLevel: Level = Level.INFO
+  protected def useParentLoggers: Boolean = true
+
+  import java.util.logging.Level._
+
+  final def finest(msg: => String): Unit = if (log.isLoggable(FINEST)) log.finest(msg)
+  final def finest(msg: Output): Unit = if (log.isLoggable(FINEST)) log.finest(msg.output)
+  final def trace(msg: => String): Unit = if (log.isLoggable(FINER)) log.finer(msg)
+  final def trace(msg: Output): Unit = if (log.isLoggable(FINER)) log.finer(msg.output)
+  final def debug(msg: => String): Unit = if (log.isLoggable(FINE)) log.fine(msg)
+  final def debug(msg: Output): Unit = if (log.isLoggable(FINE)) log.fine(msg.output)
+  final def config(msg: => String): Unit = if (log.isLoggable(CONFIG)) log.config(msg)
+  final def config(msg: Output): Unit = if (log.isLoggable(CONFIG)) log.config(msg.output)
+  final def info(msg: => String): Unit  = if (log.isLoggable(INFO))  log.info(msg)
+  final def info(msg: Output): Unit  = if (log.isLoggable(INFO))  log.info(msg.output)
+  final def warn(msg: => String): Unit  = if (log.isLoggable(WARNING))  log.warning(msg)
+  final def warn(msg: Output): Unit  = if (log.isLoggable(WARNING))  log.warning(msg.output)
+  final def severe(msg: => String): Unit = if (log.isLoggable(SEVERE)) log.severe(msg)
+  final def severe(msg: Output): Unit = if (log.isLoggable(SEVERE)) log.severe(msg.output)
+
+  import java.util.logging.Handler
+
+  final def addLogHandler(h: Handler): Unit = log.addHandler(h)
+  final def removeLogHandler(h: Handler): Unit = log.removeHandler(h)
+}
