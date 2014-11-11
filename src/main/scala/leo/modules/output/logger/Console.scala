3c8605f6116bc53c2638e22da36154109f979944
Alex <a.steen@fu-berlin.de>
2014-11-11 11:53:41 +0100

    simplified console logger, it now publishes the logs to system.err

--- src/main/scala/leo/modules/output/logger/Console.scala
+++ src/main/scala/leo/modules/output/logger/Console.scala
@@ -10,22 +10,15 @@ object Console extends Logging {
   override protected val defaultLogLevel = java.util.logging.Level.CONFIG
   override protected val useParentLoggers = false
 
-  import java.util.logging.{StreamHandler, LogRecord, Formatter}
+  import java.util.logging.{ConsoleHandler, LogRecord, Formatter}
   addLogHandler(
-    new StreamHandler {
-      setLevel(Console.defaultLogLevel)
-
+    new ConsoleHandler {
+      setLevel(defaultLogLevel)
       setFormatter(new Formatter {
         def format(record: LogRecord) = {
           s"[${record.getLevel.getLocalizedName}] \t ${record.getMessage} \n"
         }
       })
-      setOutputStream(System.out)
-
-      override def publish(record: LogRecord): Unit = {
-        super.publish(record);
-        flush();
-      }
     }
   )
 
