2aae71584c2d50ab196212859d88d2068bf64cca
Alex <a.steen@fu-berlin.de>
2014-11-13 21:31:06 +0100

    added parameter access methods for some default parameters

--- src/main/scala/leo/Configuration.scala
+++ src/main/scala/leo/Configuration.scala
@@ -1,16 +1,26 @@
 package leo
 
+import java.util.logging.Level
+
 import leo.modules.CLParameterParser
+import leo.modules.output.logger.Out
+import leo.modules.output.Output
 
 /**
- * Created by lex on 13.11.14.
+ * Configuration access point where central parameter settings of Leo-III
+ * can be accessed and read. The Configuration object needs to be initialized
+ * using the `init` method and a command line argument parser, i.e. subclasses of
+ * `CLParameterParser`,
+ *
+ * @author Alexander Steen
+ * @since 13.11.2014
  */
-object Configuration {
+object Configuration extends DefaultConfiguration {
   private var configMap: Map[String, Seq[String]] = null
-  private var parser: CLParameterParser = null
 
-  val THREADCOUNTPARAM = "n"
-  val THREADCOUNTDEFAULT = 4
+  private val PARAM_THREADCOUNT = "n"
+  private val PARAM_VERBOSITY = "v"
+  private val PARAM_TIMEOUT = "t"
 
   def init(parameterParser: CLParameterParser): Unit = configMap match {
     case null => {
@@ -18,7 +28,12 @@ object Configuration {
       for(param <- parameterParser.getParameters) {
         configMap += (param)
       }
-      parser = parameterParser
+      // Force values of lazy vals s.t. potential errors are reported as early as possible
+      PROBLEMFILE
+      THREADCOUNT
+      VERBOSITY
+      TIMEOUT
+      ()
     }
     case _ => ()
   }
@@ -27,31 +42,81 @@ object Configuration {
   // Predefined parameters
   //////////////////////////
 
-  val PROBLEMFILE: String = configMap.get(parser.ARG0Name) match {
+  lazy val PROBLEMFILE: String = configMap.get(CLParameterParser.ARG0Name) match {
     case None => ""
-    case Some(str) => str(0)
+    case Some(str :: Nil) => str
+    case Some(_) => ???
   }
 
-  val THREADCOUNT: Int = configMap.get(THREADCOUNTPARAM) match {
-    case None => THREADCOUNTDEFAULT
-    case Some(str) => try {
-      str(0).toInt
-    } catch {
-      case _:Throwable => THREADCOUNTDEFAULT
+  lazy val THREADCOUNT: Int = uniqueIntFor(PARAM_THREADCOUNT, DEFAULT_THREADCOUNT)
+
+  lazy val VERBOSITY: java.util.logging.Level = configMap.get(PARAM_VERBOSITY) match {
+    case None => DEFAULT_VERBOSITY
+    case Some(arg :: Nil) => processLevel(arg)
+    case Some(arg :: _) => Out.warn(multiDefOutput(PARAM_VERBOSITY)); processLevel(arg)
+  }
+
+  protected def processLevel(actual: String): Level = safeStrToInt(actual) match {
+    case None => DEFAULT_VERBOSITY
+    case Some(0) => Level.OFF
+    case Some(1) => Level.INFO
+    case Some(2) => Level.CONFIG
+    case Some(3) => Level.FINE
+    case Some(4) => Level.FINER
+    case Some(5) => Level.FINEST
+    case _ => {
+      Out.warn(s"Allowed verbosity levels for parameter $PARAM_VERBOSITY are integers from 0 (including) to 5 (including).");
+      DEFAULT_VERBOSITY}
+  }
+
+  lazy val TIMEOUT: Int = uniqueIntFor(PARAM_TIMEOUT, DEFAULT_TIMEOUT)
+
+
+  // Utility
+
+  protected def uniqueIntFor(param: String, default: Int): Int = configMap.get(param) match {
+    case None => default
+    case Some(arg :: Nil) => processIntFor(param, arg, default)
+    case Some(arg :: _) => {
+      Out.warn(multiDefOutput(param))
+      processIntFor(param, arg, default)
     }
   }
-  val VERBOSITY: java.util.logging.Level = ???
-  val TIMEOUT: Int = ???
+  protected def processIntFor(param: String, actual: String, default: Int): Int = {
+    safeStrToInt(actual).getOrElse({
+      Out.warn(intExpectedOutput(param, actual))
+      default})
+  }
 
   // more to come ...
 
+  protected def multiDefOutput(paramName: String): Output = new Output {
+    val output = s"Parameter $paramName was defined multiple times. First occurrence is used, the rest is ignored."
+  }
+  protected def intExpectedOutput(paramName: String, actual: String): Output = new Output {
+    val output = s"Parameter $paramName expects an Integer value, but '$actual' was given. Default value is used."
+  }
+  protected def safeStrToInt(str: String): Option[Int] = try {
+    Some(str.toInt)
+  } catch {
+    case _:Throwable => None
+  }
 
   //////////////////////////////
   // General purpose accessors
   //////////////////////////////
 
-  def isSet(param: String): Boolean = ???
-  def valueOf(param: String): Option[String] = ???
-  def isSetTo(param: String, arg: String): Boolean = ???
+  def isSet(param: String): Boolean = configMap.get(param).isDefined
+  def valueOf(param: String): Option[Seq[String]] = configMap.get(param)
+  def isSetTo(param: String, arg: String): Boolean =  configMap.get(param) match {
+    case None => false
+    case Some(args) => args.length == 1 && args(0) == arg
+  }
 
+}
+
+trait DefaultConfiguration {
+  val DEFAULT_THREADCOUNT = 4
+  val DEFAULT_VERBOSITY = java.util.logging.Level.INFO
+  val DEFAULT_TIMEOUT = 60
 }
