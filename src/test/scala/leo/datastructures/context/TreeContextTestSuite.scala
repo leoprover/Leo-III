73f87095b6c46a17809d0c08bf1ed22b184385da
Max Wisniewski <max.wisniewski@fu-berlin.de>
2014-11-25 16:41:34 +0100

    Wrote tests for the tree context

--- src/test/scala/leo/datastructures/context/TreeContextTestSuite.scala
+++ src/test/scala/leo/datastructures/context/TreeContextTestSuite.scala
@@ -1,5 +1,7 @@
 package leo.datastructures.context
 
+import leo.Configuration
+import leo.modules.CLParameterParser
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
@@ -16,10 +18,34 @@ import leo.datastructures.context.impl.TreeContext
  */
 @RunWith(classOf[JUnitRunner])
 class TreeContextTestSuite extends FunSuite{
+
   test("BaseContext empty"){
     val b : Context = new TreeContext // Creates an empty BaseContext
     assert(b.parentContext == null, "Parent Context is not empty")
     assert(b.childContext.isEmpty, "There are children in initial state.")
     assert(b.splitKind == NoSplit, "The initial context is already splitted.")
+  }
+
+  test("Creating children") {
+    val b : Context = new TreeContext
+    b.split(AlphaSplit, 3)
+    assert(b.splitKind == AlphaSplit, "The parent is not Alpha splitted.")
+    assert(b.childContext.size == 3, "The root context should have 3 children")
+    b.childContext foreach { c =>
+      assert(c.parentContext == b, s"Parent Context should be root=$b")
+      assert(c.childContext.isEmpty, "There are children in initial state.")
+      assert(c.splitKind == NoSplit, "The initial context is already splitted.")
+    }
+  }
+
+  test("Closing context") {
+    val b : Context = new TreeContext
+    b.split(AlphaSplit, 3)
+    assert(b.childContext.size == 3, "The root context should have 3 children")
+
+    b.close()
+
+    assert(b.splitKind == UnSplittable, "The root context should be closed.")
+    assert(b.childContext.size == 0, "The root context should have no children")
   }
 }
