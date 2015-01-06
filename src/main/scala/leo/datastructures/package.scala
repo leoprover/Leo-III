2f52259cfef5b14f5eee43ba1dea2e3df2d565aa
Alex <a.steen@fu-berlin.de>
2015-01-06 12:54:27 +0100

    added a package object for the datastructure package

---
+++ src/main/scala/leo/datastructures/package.scala
@@ -0,0 +1,17 @@
+package leo
+
+/**
+ * Created by lex on 06.01.15.
+ */
+package object datastructures {
+
+  def fuseMaps[A,B](map1: Map[A,Set[B]], map2: Map[A,Set[B]]): Map[A, Set[B]] = {
+    map2.foldLeft(map1)({case (intermediateMap, (k,v)) =>
+      if (!intermediateMap.contains(k))
+        intermediateMap + (k -> v)
+      else
+        intermediateMap + (k -> (intermediateMap(k) ++ v))
+    })
+  }
+
+}
