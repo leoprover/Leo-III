0f226d734092b2495ba725b65360e45be02d63e0
Max Wisniewski <ryugoron@gmx.de>
2015-01-12 17:06:50 +0100

    Implemented a simple clausification for clauses in prenex normal form.

---
+++ src/main/scala/leo/modules/proofCalculi/resolution/Clausification.scala
@@ -0,0 +1,53 @@
+package leo.modules.proofCalculi.resolution
+
+import leo.datastructures._
+import leo.datastructures.term.Term
+
+/**
+ *
+ * Performs a step wise clausification, if there are no quantifiers involved.
+ *
+ * @author Max Wisniewski
+ * @since 1/12/15
+ */
+object Clausification {
+  def clausify(c : Clause) : Seq[Clause] = {
+    val clausilits : Seq[Literal] = c.lits.filter(clausifiable)
+    if(!clausilits.isEmpty) {
+      val mLit = clausilits.max
+      val pol = mLit.polarity
+      val rLits = rmEl(c.lits, mLit)
+      mLit.term match {
+        case (a & b) if pol => return List(updateLits(c, appendLit(rLits, a,pol)), updateLits(c, appendLit(rLits, b, pol)))
+        case (a & b) => return List(updateLits(c, appendLit(appendLit(rLits, a, pol), b, pol)))
+        case (a ||| b) if !pol => return List(updateLits(c, appendLit(rLits, a,pol)), updateLits(c, appendLit(rLits, b, pol)))
+        case (a ||| b) => return List(updateLits(c, appendLit(appendLit(rLits, a, pol), b, pol)))
+        case Not(a) => return List(updateLits(c, appendLit(rLits, a, !pol)))
+        case _ => return Nil
+      }
+    }
+    Nil
+  }
+
+  private def updateLits(c : Clause, l : Seq[Literal]) : Clause = Clause.mkClause(l, c.implicitBindings, Derived)
+
+  private def appendLit(ls : Seq[Literal], t : Term, pol : Boolean) : Seq[Literal] = Literal(t, pol) +: ls
+
+  private def rmEl[A](l : Seq[A], a : A) : Seq[A]= l match {
+      case Nil => Nil
+      case (x +: xs) => if (x == a) xs else x +: rmEl(xs,a)
+    }
+
+  /**
+   * A Literal is possibly clausifiable, if it is a toplevel conjunction / disjuntion.
+   *
+   * @param l - The literal to check
+   * @return true iff it is disjuntiv / conjunctive
+   */
+  private def clausifiable(l : Literal) : Boolean = l.term match {
+    case (a & b) => true
+    case (a ||| b) => true
+    case (Not(a))   => true
+    case _        => false
+  }
+}
---
+++ src/test/scala/leo/modules/proofCalculi/ClausificationTestSuite.scala
@@ -0,0 +1,115 @@
+package leo
+package modules.proofCalculi
+
+import leo.modules.Utility._
+import org.scalatest.FunSuite
+import leo.modules.CLParameterParser
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import leo.modules.proofCalculi.resolution.Clausification
+import leo.datastructures.{Derived, Clause}
+
+/**
+ * Created by max on 12.01.2015.
+ */
+@RunWith(classOf[JUnitRunner])
+class ClausificationTestSuiteextends extends FunSuite{
+  Configuration.init(new CLParameterParser(Array("arg0", "-v", "4")))
+
+  test("And Positive") {
+    clear()
+    clearSignature()
+    add("fof(a,conjecture,r&(p&q)).")
+    add("fof(l, axiom, r).")
+    add("fof(r, axiom, (p&q)).")
+    val f = get("a")
+    val l = get("l").clause
+    val r = get("r").clause
+    val nc = Clausification.clausify(f.clause)
+
+    assert(testRes(nc,l), "The clausification should contain \n"++l.pretty++", but was \n"++nc.map(_.pretty).mkString(", "))
+    assert(testRes(nc,r), "The clausification should contain \n"++r.pretty++", but was \n"++nc.map(_.pretty).mkString(", "))
+  }
+
+  test("And Negative") {
+    clear()
+    clearSignature()
+    add("fof(a,conjecture,r&(p&q)).")
+    add("fof(l, axiom, r).")
+    add("fof(r, axiom, (p&q)).")
+    val f = get("a").clause.mapLit(_.flipPolarity)  // Flip the polarity for test
+    val r = Clause.mkClause(get("l").clause.lits++get("r").clause.lits, Nil, Derived).mapLit(_.flipPolarity)
+
+    val nc = Clausification.clausify(f)
+
+    assert(testRes(nc, r), "The clausification should contain \n"++r.pretty++", but was \n"++nc.map(_.pretty).mkString(", "))
+  }
+
+  test("Or Positive") {
+    clear()
+    clearSignature()
+    add("fof(a,conjecture,r | (p&q)).")
+    add("fof(l, axiom, r).")
+    add("fof(r, axiom, (p&q)).")
+    val f = get("a").clause  // Flip the polarity for test
+    val r = Clause.mkClause(get("l").clause.lits++get("r").clause.lits, Nil, Derived)
+
+    val nc = Clausification.clausify(f)
+
+    assert(testRes(nc, r), "The clausification should contain \n"++r.pretty++", but was \n"++nc.map(_.pretty).mkString(", "))
+  }
+
+  test("Or Negative") {
+    clear()
+    clearSignature()
+    add("fof(a,conjecture,r | (p&q)).")
+    add("fof(l, axiom, r).")
+    add("fof(r, axiom, (p&q)).")
+
+    val f = get("a").clause.mapLit(_.flipPolarity)
+    val l = get("l").clause.mapLit(_.flipPolarity)
+    val r = get("r").clause.mapLit(_.flipPolarity)
+    val nc = Clausification.clausify(f)
+
+    assert(testRes(nc,l), "The clausification should contain \n"++l.pretty++", but was \n"++nc.map(_.pretty).mkString(", "))
+    assert(testRes(nc,r), "The clausification should contain \n"++r.pretty++", but was \n"++nc.map(_.pretty).mkString(", "))
+  }
+
+  test("Not Positve") {
+    clear()
+    clearSignature()
+    add("fof(a,conjecture,~q).")
+    add("fof(l, axiom, q).")
+
+    val f = get("a").clause
+    val r = get("l").clause.mapLit(_.flipPolarity)
+
+    val nc = Clausification.clausify(f)
+
+    assert(testRes(nc, r), "The clausification should contain \n"++r.pretty++", but was \n"++nc.map(_.pretty).mkString(", "))
+  }
+
+  test("Not Negaitve") {
+    clear()
+    clearSignature()
+    add("fof(a,conjecture,~q).")
+    add("fof(l, axiom, q).")
+
+    val f = get("a").clause.mapLit(_.flipPolarity)
+    val r = get("l").clause
+
+    val nc = Clausification.clausify(f)
+
+    assert(testRes(nc, r), "The clausification should contain \n"++r.pretty++", but was \n"++nc.map(_.pretty).mkString(", "))
+  }
+
+  private def testRes(c1 : Seq[Clause], c2 : Clause) : Boolean = c1 exists(cmpClause(_,c2))
+
+  private def cmpClause(c1 : Clause, c2 : Clause) : Boolean = {
+    c1.lits forall { l1 =>
+      c2.lits exists {l2 =>
+        l1.polarity == l2.polarity && l1.term == l2.term
+      }
+    }
+  }
+}
