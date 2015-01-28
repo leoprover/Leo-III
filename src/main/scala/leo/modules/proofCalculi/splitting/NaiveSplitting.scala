9386afc4c9d5dbe984561c7af42495fbf37991c7
Max Wisniewski <ryugoron@gmx.de>
2015-01-28 13:55:10 +0100

    Started implementing a splitting rule.

---
+++ src/main/scala/leo/modules/proofCalculi/splitting/NaiveSplitting.scala
@@ -0,0 +1,12 @@
+package leo.modules.proofCalculi.splitting
+
+/**
+ * First implementation of a splitting rule.
+ * Scans for easy split cases and returns them.
+ *
+ * @author Max Wisniewski
+ * @since 28/1/15
+ */
+object NaiveSplitting {
+  
+}
