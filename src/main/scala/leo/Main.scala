fc27f17f4c9c7b3f95ac850d7025b4ba96e903d6
Alex <a.steen@fu-berlin.de>
2015-04-09 17:01:30 +0200

    added outermost exception catching to the main of leo

--- src/main/scala/leo/Main.scala
+++ src/main/scala/leo/Main.scala
@@ -7,11 +7,10 @@ import leo.agents.impl.{CounterContextControlAgent, ContextControlAgent}
 import leo.datastructures.blackboard.Blackboard
 import leo.datastructures.blackboard.scheduler.Scheduler
 import leo.datastructures.context.Context
-import leo.modules.{Utility, SZSOutput, CLParameterParser}
+import leo.modules._
 import leo.modules.Utility._
-import leo.modules.output.{SZS_Unknown, SZS_GaveUp, SZS_Unsatisfiable, SZS_Timeout}
+import leo.modules.output._
 import leo.modules.Phase._
-import leo.modules.Phase
 
 
 /**
@@ -31,64 +30,94 @@ object Main {
    * @param args - See [[Configuration]] for argument treatment
    */
   def main(args : Array[String]){
-    val beginTime = System.currentTimeMillis()
     try {
-      Configuration.init(new CLParameterParser(args))
-    } catch {
-      case e:IllegalArgumentException => {
-        Out.severe(e.getMessage)
+      val beginTime = System.currentTimeMillis()
+      try {
+        Configuration.init(new CLParameterParser(args))
+      } catch {
+        case e: IllegalArgumentException => {
+          Out.severe(e.getMessage)
+          Configuration.help()
+          return
+        }
+      }
+      if (Configuration.HELP) {
         Configuration.help()
         return
       }
+
+      val timeout = if (Configuration.TIMEOUT == 0) Double.PositiveInfinity else Configuration.TIMEOUT
+      val interval = 10
+
+
+      val deferredKill: DeferredKill = new DeferredKill(interval, timeout)
+      deferredKill.start()
+
+      // Create Scheduler
+      Scheduler(Configuration.THREADCOUNT)
+
+      var it: Iterator[Phase] = null
+      if (Configuration.COUNTER_SAT) {
+        CounterContextControlAgent.register()
+        it = getCounterSat.iterator
+      } else if (Configuration.isSet("with-prover")) {
+        ContextControlAgent.register()
+        it = getExternalPhases.iterator
+      } else {
+        ContextControlAgent.register()
+        it = getHOStdPhase.iterator
+      }
+      var r = true
+      while (it.hasNext && r && !deferredKill.finished) {
+        val phase = it.next()
+        Out.info(s"\n [Phase]:\n  Starting ${phase.name}\n${phase.description}")
+        val start = System.currentTimeMillis()
+        r = phase.execute()
+        val end = System.currentTimeMillis()
+        Out.info(s"\n [Phase]:\n  Ended ${phase.name}\n  Time: ${end - start}ms")
+      }
+      deferredKill.kill()
+
+      Out.output(s"% SZS status ${Blackboard().getStatus(Context()).fold(SZS_Unknown.output)(_.output)} for ${Configuration.PROBLEMFILE}")
+      if (Configuration.PROOF_OBJECT) Blackboard().getAll { p => p.clause.isEmpty}.foreach(Utility.printDerivation(_))
+      val endTime = System.currentTimeMillis()
+      //    Out.output("Main context "+Context().contextID)
+      //    formulaContext(Context())
+      //    for(c <- Context().childContext){
+      //      formulaContext(c)
+      //    }
+      Out.output("% Time: " + (endTime - beginTime) + "ms")
+
+    } catch {
+      case e:SZSException => {
+        if (e.getMessage != null) {
+          Out.comment(e.getMessage)
+          Out.info(e.getMessage)
+        }
+        Out.output(SZSOutput(e.status, Configuration.PROBLEMFILE))
+        Out.debug(e.debugMessage)
+        Out.trace(stackTraceAsString(e))
+        if (e.getCause != null) {
+          Out.trace("Caused by: " + e.getCause.getMessage)
+          Out.trace("at: " + e.getCause.getStackTrace.toString)
+        }
+      }
+      case e:Throwable => {
+        if (e.getMessage != null) {
+          Out.comment(e.getMessage)
+          Out.info(e.getMessage)
+        }
+        Out.output(SZSOutput(SZS_Error, Configuration.PROBLEMFILE))
+        Out.trace(stackTraceAsString(e))
+        if (e.getCause != null) {
+          Out.trace("Caused by: " + e.getCause.getMessage)
+          Out.trace("at: " + e.getCause.getStackTrace.toString)
+        }
+      }
+    } finally {
+      Scheduler().killAll()
+      System.exit(0)
     }
-    if (Configuration.HELP) {
-      Configuration.help()
-      return
-    }
-
-    val timeout = if (Configuration.TIMEOUT == 0) Double.PositiveInfinity else Configuration.TIMEOUT
-    val interval = 10
-
-
-    val deferredKill : DeferredKill = new DeferredKill(interval, timeout)
-    deferredKill.start()
-
-    // Create Scheduler
-    Scheduler(Configuration.THREADCOUNT)
-
-    var it : Iterator[Phase] = null
-    if(Configuration.COUNTER_SAT){
-      CounterContextControlAgent.register()
-      it = getCounterSat.iterator
-    } else if (Configuration.isSet("with-prover")) {
-      ContextControlAgent.register()
-      it = getExternalPhases.iterator
-    } else {
-      ContextControlAgent.register()
-      it = getHOStdPhase.iterator
-    }
-    var r = true
-    while(it.hasNext && r && !deferredKill.finished) {
-      val phase = it.next()
-      Out.info(s"\n [Phase]:\n  Starting ${phase.name}\n${phase.description}")
-      val start = System.currentTimeMillis()
-      r = phase.execute()
-      val end = System.currentTimeMillis()
-      Out.info(s"\n [Phase]:\n  Ended ${phase.name}\n  Time: ${end-start}ms")
-    }
-    deferredKill.kill()
-
-    Out.output(s"% SZS status ${Blackboard().getStatus(Context()).fold(SZS_Unknown.output)(_.output)} for ${Configuration.PROBLEMFILE}")
-    if(Configuration.PROOF_OBJECT) Blackboard().getAll{p => p.clause.isEmpty}.foreach(Utility.printDerivation(_))
-    val endTime = System.currentTimeMillis()
-//    Out.output("Main context "+Context().contextID)
-//    formulaContext(Context())
-//    for(c <- Context().childContext){
-//      formulaContext(c)
-//    }
-    Out.output("% Time: "+(endTime - beginTime)+"ms")
-    Scheduler().killAll()
-    System.exit(0);
   }
 
 
