8cb2a95c6603381122d317c0a65e2e23dbef691e
Alex <a.steen@fu-berlin.de>
2015-05-19 12:08:09 +0200

    new not-working bool-ext rule

--- src/main/scala/leo/modules/proofCalculi/Paramodulation.scala
+++ src/main/scala/leo/modules/proofCalculi/Paramodulation.scala
@@ -428,6 +428,56 @@ object BoolExt extends UnaryCalculusRule[Clause, (Seq[Literal], Seq[Literal])] {
 
   def name = "bool_ext"
 }
+/** alternative boolean extensionality rule:
+  * {{{
+  *   C \/ [s1_o = t1_o]^alpha1 \/ [s2_o = t2_o]^alpha2 \/ .... \/ [sn_o = tn_o]^alphan
+  * --------------------------------------------------------------------------------------
+  *   C \/ [s1]^t \/ [t1]^f \/ [s2]^t \/ [t2]^f ... \/ [sn]^t \/ [tn]^f
+  *   ....
+  *   C \/ [s1]^f \/ [t1]^t \/ [s2]^f \/ [t2]^t ... \/ [sn]^f \/ [tn]^t
+  * }}}
+  *
+  * That are, n^2^ many new clauses
+  * */
+object BoolExtAlt extends UnaryCalculusRule[Set[Clause], (Seq[Literal], Seq[Literal])] {
+  def canApply(cl: Clause): (Boolean, (Seq[Literal], Seq[Literal])) = {
+    var it = cl.lits.iterator
+    var boolExtLits: Seq[Literal] = Seq()
+    var otherLits: Seq[Literal] = Seq()
+    while (it.hasNext) {
+      val lit = it.next()
+      import leo.datastructures.impl.Signature
+      lit.term match {
+        case (left === _) if left.ty == Signature.get.o => boolExtLits = boolExtLits :+ lit
+        case _ => otherLits = otherLits :+ lit
+      }
+    }
+    (boolExtLits.nonEmpty, (boolExtLits, otherLits))
+  }
+
+  def apply(v1: Clause, boolExtLits_otherLits: (Seq[Literal], Seq[Literal])) = {
+    val boolExtLits = boolExtLits_otherLits._1
+    val otherLits = boolExtLits_otherLits._2
+    var groundLits: Seq[Seq[Literal]] = Seq(otherLits)
+    val it = boolExtLits.iterator
+    while (it.hasNext) {
+      val lit = it.next()
+      val (left, right) = ===.unapply(lit.term).get
+      var newGroundLits : Seq[Seq[Literal]] = Seq()
+      groundLits.foreach( lits =>
+        newGroundLits = newGroundLits :+ (lits :+ Literal.mkNegLit(left) :+ Literal.mkPosLit(right))
+                                :+ (lits :+ Literal.mkPosLit(left) :+ Literal.mkNegLit(right))
+      )
+      groundLits = newGroundLits
+//      groundLits = groundLits :+ Literal.mkLit(<=>(left,right).full_δ_expand.betaNormalize, lit.polarity)
+    }
+    groundLits.map(lits => Clause.mkClause(lits, Derived)).toSet
+//    NegationNormal.normalize(Simplification.normalize(Clause.mkClause(otherLits ++ groundLits, Derived)))
+  }
+
+  def name = "bool_ext_alt"
+}
+
 
 object FuncExt extends UnaryCalculusRule[Clause, (Seq[Literal], Seq[Literal])] {
   def canApply(cl: Clause): (Boolean, (Seq[Literal], Seq[Literal])) = {
