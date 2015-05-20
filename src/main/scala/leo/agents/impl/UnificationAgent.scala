d31d002398b0bdee86c1ced97a8a205d3d75d781
Max Wisniewski <ryugoron@gmx.de>
2015-05-20 14:08:49 +0200

    Adjusted unification agent to new structure

--- src/main/scala/leo/agents/impl/UnificationAgent.scala
+++ src/main/scala/leo/agents/impl/UnificationAgent.scala
@@ -4,7 +4,7 @@ package impl
 
 import leo.datastructures._
 import leo.datastructures.blackboard._
-import leo.datastructures.blackboard.impl.{UnificationStore, UnifierType, UnifierStore}
+import leo.datastructures.blackboard.impl.{UnificationTaskType, UnificationStore, UnifierType, UnifierStore}
 
 /**
  *
@@ -16,16 +16,19 @@ import leo.datastructures.blackboard.impl.{UnificationStore, UnifierType, Unifie
  */
 class UnificationAgent extends Agent {
   override def name: String = "unification agent"
-  override val interest = Some(List(FormulaType, UnifierType))
+  override val interest = Some(List(UnificationTaskType, UnifierType))
 
   /**
    * This function runs the specific agent on the registered Blackboard.
    */
   override def run(t: Task): Result = t match {
     case UnificationTask(f,t1,t2,s) =>
-      Out.trace(s"[$name]: Run unification task on clause ${f.clause.pretty}: \n unify ${t1.pretty} and ${t2.pretty} with \n ${s.pretty}")
-      val nf = Store(f.clause.substitute(s), Role_Plain, f.context, f.status)
-      Result().insert(FormulaType)(nf).insert(UnifierType)(UnifierStore(f,t1,t2,s))
+      trace(s"Run unification task on clause ${f.clause.pretty}: \n unify ${t1.pretty} and ${t2.pretty} with \n ${s.pretty}")
+      val nf = Store(f.clause.substitute(s), Role_Plain, f.context, f.status) // TODO apply triv rules and replace [t = t] = b with [true] = b
+      Result().insert(UnificationTaskType)(nf).insert(UnifierType)(UnifierStore(f,t1,t2,s))
+    case FinishUnify(f) =>
+      trace(s"Finished unification. Inserted\n   ${f.pretty}\ninto active.")
+      Result().insert(FormulaType)(f)
     case _ => Result()
   }
 
@@ -35,12 +38,14 @@ class UnificationAgent extends Agent {
    * b) a performed unification, if there could be applied another unifier
    */
   override def toFilter(event: Event): Iterable[Task] = event match {
-    case DataEvent(f : FormulaStore, FormulaType) =>
+    case DataEvent(f : FormulaStore, UnificationTaskType) =>
+      if(f.clause.uniLits.isEmpty) return List(FinishUnify(f))
       f.clause.uniLits.map{ l => l.eqComponents match {
         case Some((t1,t2))  => UnificationStore.nextUnifier(f,t1,t2).fold(Nil : List[Task]){s => List(UnificationTask(f,t1,t2,s))}
         case _  => Nil
       }}.flatten
     case DataEvent(UnifierStore(f, t1, t2, subst), UnifierType) => // Executed One, take the next one
+      if(f.clause.uniLits.isEmpty) return List(FinishUnify(f)) // Should not happen
       UnificationStore.nextUnifier(f,t1,t2).fold(Nil : List[Task]){s => List(UnificationTask(f,t1,t2,s))}
     case _ => Nil
   }
@@ -51,7 +56,16 @@ private case class UnificationTask(f : FormulaStore, t1 : Term, t2 : Term, s : S
   override def name: String = "unification task"
   override def writeSet(): Set[FormulaStore] = Set()
   override def readSet(): Set[FormulaStore] = Set(f)
-  override def bid(budget: Double): Double = budget / 10
+  override def bid(budget: Double): Double = budget / (1+f.clause.uniLits.size)
+
+  override def pretty: String = "unify"
+}
+
+private case class FinishUnify(f : FormulaStore) extends Task {
+  override def name : String = "finish unification task"
+  override def writeSet(): Set[FormulaStore] = Set()
+  override def readSet(): Set[FormulaStore] = Set(f)
+  override def bid(budget: Double): Double = budget / 2
 
   override def pretty: String = "unify"
 }
