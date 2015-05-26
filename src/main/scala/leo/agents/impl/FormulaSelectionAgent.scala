5d4174a3ba677c23bc8637031a2326e55ebde3c5
Alex <a.steen@fu-berlin.de>
2015-05-26 13:11:44 +0200

    added backward subsumption to formulaselection agent. forward subsumption still as comment

--- src/main/scala/leo/agents/impl/FormulaSelectionAgent.scala
+++ src/main/scala/leo/agents/impl/FormulaSelectionAgent.scala
@@ -5,7 +5,7 @@ package impl
 import leo.datastructures.TimeStamp
 import leo.datastructures.blackboard.impl.{SelectionTimeStore, TimeData, SelectionTimeType}
 import leo.datastructures.blackboard._
-import leo.modules.calculus.{FuncExt, BoolExt, StdPrimSubst}
+import leo.modules.calculus.{Subsumption, FuncExt, BoolExt, StdPrimSubst}
 
 /**
  * Created by lex on 20.05.15.
@@ -18,7 +18,19 @@ object FormulaSelectionAgent extends Agent {
     t match {
       case AddTimeStampTask(f) =>
         //if(f.clause.lits.size == 1) comment(s"Has selected clause ${f.pretty}.(weight=${t.bid(100)})")
-        Result().insert(SelectionTimeType)(TimeData(f, TimeStamp()))
+        val r = Result()
+        r.insert(SelectionTimeType)(TimeData(f, TimeStamp()))
+        /* remove subsumed clauses from active */
+//        val remove = SelectionTimeStore.noSelect(f.context).filterNot(f2 => Subsumption.subsumes(f2.clause, f.clause))
+//        if (remove.nonEmpty) trace(s"Found by ${f.pretty} subsumable clauses in active, remove them.")
+//        remove.foreach {case fs =>
+//        r.remove(FormulaType)(fs)
+//        }
+        /* */
+        r
+      case RemoveFormulaTask(f) =>
+        trace(s"Removed ${f.pretty}.")
+        Result().remove(FormulaType)(f)
       case _ => Out.warn(s"[$name]: Got a wrong task to execute."); Result()
     }
   }
@@ -27,10 +39,14 @@ object FormulaSelectionAgent extends Agent {
     event match {
       case DataEvent(f: FormulaStore, FormulaType) => {
         // new formula, add timestamp to selectiontimestore
-        if (!SelectionTimeStore.get(f).isDefined /*&& !BoolExt.canApply(f.clause)._1 && !FuncExt.canApply(f.clause)._1*/) {
-          val t = AddTimeStampTask(f)
-          //if(f.clause.lits.size == 1) comment(s"Try select clause ${f.pretty}.(weight=${t.bid(100)})")
-          Seq(t)
+        if (!SelectionTimeStore.get(f).isDefined) {
+          // Not yet paramodulated
+          if (!Subsumption.canApply(f.clause, SelectionTimeStore.wasSelected(f.context).map(_.clause).toSet)) {
+            Seq(AddTimeStampTask(f))
+          } else {
+            trace(s"Found subsumed clause ${f.pretty}, create remove task")
+            Seq(RemoveFormulaTask(f))
+          }
         } else {
           Seq()
         }
@@ -39,6 +55,13 @@ object FormulaSelectionAgent extends Agent {
     }
   }
 
+  final private case class RemoveFormulaTask(f: FormulaStore) extends Task {
+    val name = "RemoveFormulaTask"
+    def writeSet() = Set(f)
+    def readSet() = Set()
+    def bid(budget: Double) = budget/f.clause.weight
+    lazy val pretty = s"Task: Remove subsumed formula ${f.pretty}"
+  }
 
   final private case class AddTimeStampTask(f: FormulaStore) extends Task {
     val name = "AddTimeStampTask"
