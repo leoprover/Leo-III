4383e79b72a7bcb3c15e8032501b28d27d042c62
Jentsch <d.jentsch@fu-berlin.de>
2015-08-30 15:56:46 +0200

    Fix simplification test

--- src/test/scala/leo/modules/normalization/SimplificationTestSuite.scala
+++ src/test/scala/leo/modules/normalization/SimplificationTestSuite.scala
@@ -1,52 +1,49 @@
 package leo.modules.normalization
 
-import leo.{Checked, LeoTestSuite}
+import leo.datastructures.Term._
 import leo.datastructures._
-import leo.datastructures.impl.Signature
-import scala.collection.immutable.HashMap
-import Term._
+import leo.{Checked, LeoTestSuite}
+import org.scalatest.Matchers
 
-/**
- * Created by Max Wisniewski on 6/10/14.
- */
-class SimplificationTestSuite extends LeoTestSuite {
+class SimplificationTestSuite extends LeoTestSuite with Matchers {
   val s = getFreshSignature
 
-  val p = mkAtom(s.addUninterpreted("p",s.o))
+  val p = mkAtom(s.addUninterpreted("p", s.o))
   val q = mkAtom(s.addUninterpreted("q", s.o))
 
-  val toSimpl : Map[Term,Term] = Map[Term, Term](
-    (Not(LitTrue()),LitFalse()),
+  val toSimplify: Map[Term, Term] = Map[Term, Term](
+    (Not(LitTrue()), LitFalse()),
     (Not(LitFalse()), LitTrue()),
-    (&(p,p),p),
-    (&(p,Not(p)), LitFalse()),
-    (<=>(p,p), LitTrue()),
-    (&(p,LitTrue()), p),
-    (&(p,LitFalse()), LitFalse()),
-    (Impl(p,LitTrue()), LitTrue()),
-    (Impl(p,LitFalse()), Not(p)),
-    (<=>(p,LitTrue()), p),
-    (<=>(p,LitFalse()), Not(p)),
-    (|||(p,p), p),
-    (|||(p,Not(p)), LitTrue()),
-    (Impl(p,p), LitTrue()),
-    (|||(p,LitTrue()), LitTrue()),
-    (|||(p,LitFalse()), p),
+    (&(p, p), p),
+    (&(p, Not(p)), LitFalse()),
+    (<=>(p, p), LitTrue()),
+    (&(p, LitTrue()), p),
+    (&(p, LitFalse()), LitFalse()),
+    (Impl(p, LitTrue()), LitTrue()),
+    (Impl(p, LitFalse()), Not(p)),
+    (<=>(p, LitTrue()), p),
+    (<=>(p, LitFalse()), Not(p)),
+    (|||(p, p), p),
+    (|||(p, Not(p)), LitTrue()),
+    (Impl(p, p), LitTrue()),
+    (|||(p, LitTrue()), LitTrue()),
+    (|||(p, LitFalse()), p),
     (Impl(LitTrue(), p), p),
     (Impl(LitFalse(), p), LitTrue()),
-    (Forall(mkTermAbs(s.o,p)),p),
-    (Exists(mkTermAbs(s.o, p)),p),
-    (Forall(mkTermAbs(s.o, <=>(mkTermApp(p, mkBound(s.o,1)), mkTermApp(p, mkBound(s.o,1))))), LitTrue())
+    (Forall(mkTermAbs(s.o, p)), p),
+    (Exists(mkTermAbs(s.o, p)), p),
+    (Forall(mkTermAbs(s.o, <=>(mkTermApp(p, mkBound(s.o, 1)), mkTermApp(p, mkBound(s.o, 1))))), LitTrue())
   )
 
-//  println("\n-------------------\nSimplification Test.\n---------------------------")
-  for ((t,t1) <- toSimpl){
-    test("Simplification Test: "+t.pretty, Checked) {
-      val st = Simplification.normalize(termToClause(t)).lits.head.term
-      println("Simplicifcation: '" + t.pretty + "' was simplified to '" + st.pretty)
-      assert(st == t1, "\nThe simplified Term '" + t.pretty + "' should be '" + t1.pretty + "', but was '" + st.pretty + "'.")
+  for ((example, expectedResult) <- toSimplify) {
+    test("Simplification Test: " + example.pretty, Checked) {
+      simplify(example) should be(expectedResult)
     }
   }
 
-  def termToClause(t : Term) : Clause = Clause.mkClause(List(Literal(t, true)),Derived)
+  def simplify(t: Term): Term =
+    Simplification.normalize(termToClause(t)).lits.head.term
+
+  def termToClause(t: Term): Clause =
+    Clause.mkClause(List(Literal(t, true)), Derived)
 }
