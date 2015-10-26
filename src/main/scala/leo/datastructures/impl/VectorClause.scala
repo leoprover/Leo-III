143ab4bf3fea48fcda0c2d73c3cc8729525f65ec
Alexander Steen <a.steen@fu-berlin.de>
2015-10-26 15:04:58 +0100

    some progress for implementing new clauses

--- src/main/scala/leo/datastructures/impl/VectorClause.scala
+++ src/main/scala/leo/datastructures/impl/VectorClause.scala
@@ -2,31 +2,36 @@ package leo.datastructures.impl
 
 import leo.datastructures._
 
+
+// TODO
+// Keep meta vars, transform to bound? transform bound to meta?
 /**
  * Preliminary implementation of clauses using indexed linear sequences (vectors).
  *
  * @author Alexander Steen
  * @since 23.11.2014
  */
-abstract sealed class VectorClause extends Clause {
-  lazy val flexHeadLits: Set[Literal] = lits.filter(_.flexHead).toSet
-  lazy val uniLits: Set[Literal] = lits.filter(_.isUni).toSet
-  lazy val freeVars: Set[Term] = lits.map(_.term.freeVars).flatten.toSet
-  lazy val eqLits: Set[Literal] = lits.filter(_.isEq).toSet
+ abstract sealed class VectorClause extends Clause {
+  /** The types of the implicitly universally quantified variables. */
+  final val implicitlyBound: Set[(Type, Int)] = lits.map(_.metaVars).toSet.flatten
+
+  /** Those literals in `lits` that are positive. */
+  final val posLits: Seq[Literal] = lits.filter(_.polarity)
+  /** Those literals in `lits` that are negative. */
+  final val negLits: Seq[Literal] = lits.filter(!_.polarity)
 }
 
 object VectorClause {
   private var clauseCounter : Int = 0
 
-  def mkClause(lits: Iterable[Literal], implicitBindings: Seq[Type], origin: ClauseOrigin): Clause = {
+  final def mkClause(lits: Iterable[Literal], origin: ClauseOrigin): Clause = {
     clauseCounter += 1
-    new VectorClause0(lits, origin, implicitBindings, clauseCounter)
+    new VectorClause0(clauseCounter, lits, origin)
   }
 
+  @inline final def lastClauseId = clauseCounter
 
-  def lastClauseId = clauseCounter
-
-  private class VectorClause0(literals: Iterable[Literal], val origin: ClauseOrigin, val implicitBindings: Seq[Type], val id: Int) extends VectorClause {
-    lazy val lits = literals.toVector.sorted
+  private class VectorClause0(val id: Int, literals: Iterable[Literal], val origin: ClauseOrigin) extends VectorClause {
+    lazy val lits = literals.toVector
   }
 }
