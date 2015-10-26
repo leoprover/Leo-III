414b965ba83bf265df7e6a7c51c0309bf7acbf50
Alexander Steen <a.steen@fu-berlin.de>
2015-10-26 15:02:26 +0100

    removed ordering values from package pbjects

--- src/main/scala/leo/package.scala
+++ src/main/scala/leo/package.scala
@@ -1,20 +1,7 @@
 import leo.datastructures._
 
 package object leo {
-  type CompareResult = Byte
 
-  /** Unknown compare status. */
-  final val CMP_UK: CompareResult = 0.toByte
-  /** Not comparable. */
-  final val CMP_NC: CompareResult = 1.toByte
-  /** Equal by comparison */
-  final val CMP_EQ: CompareResult = 2.toByte
-  /** (strictly) less. */
-  final val CMP_LT: CompareResult = 3.toByte
-  /** (strictly) greater. */
-  final val CMP_GT: CompareResult = 4.toByte
-
-  type TypeOrdering = QuasiOrdering[Type]
   type TermOrdering = leo.datastructures.impl.orderings.TO_CPO_Naive.type // FIXME Hacky
   type ClauseOrdering = Ordering[Clause]
   type LiteralOrdering = Ordering[Literal]
