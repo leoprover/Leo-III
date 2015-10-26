1ea8bb6ee4e3b233a255d3de70f3df5b9e2ac171
Alexander Steen <a.steen@fu-berlin.de>
2015-10-26 15:02:54 +0100

    removed crap interfaces from orderings package

--- src/main/scala/leo/datastructures/Orderings.scala
+++ src/main/scala/leo/datastructures/Orderings.scala
@@ -18,11 +18,21 @@ import scala.annotation.tailrec
 /////////////////////
 
 object Orderings {
-  import scala.language.implicitConversions
 
-  def isComparable(x: CompareResult): Boolean = (x & ~(CMP_EQ | CMP_UK)) != 0
-  def isGE(x: CompareResult): Boolean = (x & (CMP_EQ | CMP_GT)) != 0
-  def isLE(x: CompareResult): Boolean = (x & (CMP_EQ | CMP_LT)) != 0
+  type CMP_Result = Byte
+  final val CMP_EQ: CMP_Result = 0.toByte
+  final val CMP_LT: CMP_Result = 1.toByte
+  final val CMP_GT: CMP_Result = 2.toByte
+  final val CMP_NC: CMP_Result = 3.toByte
+
+  @inline final def isComparable(x: CMP_Result): Boolean = (x & ~CMP_EQ) != 0
+  @inline final def isGE(x: CMP_Result): Boolean = (x & (CMP_EQ | CMP_GT)) != 0
+  @inline final def isLE(x: CMP_Result): Boolean = (x & (CMP_EQ | CMP_LT)) != 0
+  @inline final def invCMPRes(x: CMP_Result): CMP_Result = {
+    if (x == CMP_GT) CMP_LT
+    else if (x == CMP_LT) CMP_GT
+    else x
+  }
 
   /** Return a (simple) ordering that is induced by a weighting. */
   def simple[A](weighting: Weight[A]) = new Ordering[A] {
@@ -36,50 +46,19 @@ object Orderings {
   def lift[A](f: A => A => Int): Ordering[A] = new Ordering[A] {
     def compare(x: A, y: A) = f(x)(y)
   }
-  def lift[A](f:  (A,A) => Option[Int]): QuasiOrdering[A] = new QuasiOrdering[A] {
-    def compare(x: A, y: A) = f(x,y)
-  }
 
-  def productOrd[A,B](ordA: QuasiOrdering[A], ordB: QuasiOrdering[B]): QuasiOrdering[(A,B)] = new QuasiOrdering[(A, B)] {
-    def compare(x: (A, B), y: (A, B)) = ordA.compare(x._1, y._1) match {
-      case Some(0) => ordB.compare(x._2, y._2)
-      case res => res
-    }
-  }
-  def productOrd[A,B,C](ordA: QuasiOrdering[A], ordB: QuasiOrdering[B], ordC: QuasiOrdering[C]): QuasiOrdering[(A,B,C)] = new QuasiOrdering[(A, B, C)] {
-    def compare(x: (A, B, C), y: (A, B, C)) = ordA.compare(x._1, y._1) match {
-      case Some(0) => ordB.compare(x._2, y._2) match {
-        case Some(0) => ordC.compare(x._3, y._3)
-        case res => res
-      }
-      case res => res
-    }
-  }
-  def productOrd[A,B,C,D](ordA: QuasiOrdering[A], ordB: QuasiOrdering[B], ordC: QuasiOrdering[C], ordD: QuasiOrdering[D]): QuasiOrdering[(A,B,C,D)] = new QuasiOrdering[(A, B, C,D)] {
-    def compare(x: (A, B, C,D), y: (A, B, C,D)) = ordA.compare(x._1, y._1) match {
-      case Some(0) => ordB.compare(x._2, y._2) match {
-        case Some(0) => ordC.compare(x._3, y._3) match {
-          case Some(0) => ordD.compare(x._4, y._4)
-          case res => res
-        }
-        case res => res
-      }
-      case res => res
-    }
-  }
-
-  def lexOrd[A](ord: QuasiOrdering[A]): QuasiOrdering[Seq[A]] = new QuasiOrdering[Seq[A]] {
-    def compare(x: Seq[A], y: Seq[A]) = (x.length - y.length) match {
-      case 0 => (x,y) match {
-        case (Seq(), Seq()) => Some(0)
-        case (Seq(xHead, xTail@_*), Seq(yHead, yTail@_*)) => ord.compare(xHead, yHead) match {
-          case Some(0) => compare(xTail, yTail)
-          case res => res
-        }
-      }
-      case res => Some(res)
-    }
-  }
+//  def lexOrd[A](ord: QuasiOrdering[A]): QuasiOrdering[Seq[A]] = new QuasiOrdering[Seq[A]] {
+//    def compare(x: Seq[A], y: Seq[A]) = (x.length - y.length) match {
+//      case 0 => (x,y) match {
+//        case (Seq(), Seq()) => Some(0)
+//        case (Seq(xHead, xTail@_*), Seq(yHead, yTail@_*)) => ord.compare(xHead, yHead) match {
+//          case Some(0) => compare(xTail, yTail)
+//          case res => res
+//        }
+//      }
+//      case res => Some(res)
+//    }
+//  }
 
   final def mult[A](gt: (A,A) => Boolean): Seq[A] => Seq[A] => Boolean = {
     s => t => {
@@ -110,10 +89,6 @@ object Orderings {
       mult0(gt, s.tail,keepT)
     } else false
   }
-
-  implicit def toQuasiOrdering[A](ord: Ordering[A]): QuasiOrdering[A] = new QuasiOrdering[A] {
-    def compare(x: A, y: A) = Some(ord.compare(x,y))
-  }
 }
 
 
@@ -122,6 +97,7 @@ object Orderings {
 /////////////////////
 
 trait Precedence {
+  import leo.datastructures.Orderings._
   type Const = Signature#Key
   def compare(x: Const, y: Const): CMP_Result
   def gt(x: Const, y: Const): Boolean = compare(x,y) == CMP_GT
@@ -151,9 +127,9 @@ object Precedence {
 ///////////////////////
 
 object TermOrdering {
-  import leo.datastructures.impl.orderings._
-  final val senseless: TermOrdering = TO_SenselessOrdering
-  final val sizeBased: TermOrdering = TO_SizedBased
+//  import leo.datastructures.impl.orderings._
+//  final val senseless: TermOrdering = TO_SenselessOrdering
+//  final val sizeBased: TermOrdering = TO_SizedBased
 }
 
 /////////////////////
@@ -176,110 +152,22 @@ object ClauseOrdering {
 // Associated traits
 //////////////////////
 
-/**
- * A trait for ordering-like relations. Operations
- * for the relation and its reflexive closure.
- */
-trait OrderingLike[A] extends Equiv[A] {
-  def compare(x: A, y: A): CompareResult = {
-    if (equiv(x,y)) CMP_EQ
-    else if (lteq(x,y)) CMP_LT /* strictly larger since equality is handled above */
-    else if (gteq(x,y)) CMP_GT /* see above */
+trait LeoOrdering[A] {
+  import leo.datastructures.Orderings._
+
+  def gt(s: A, t: A): Boolean
+  def gteq(s: A, t: A): Boolean
+
+  // Defined by gt/ge
+  @inline final def lt(s: A, t: A): Boolean = gt(t,s)
+  @inline final def lteq(s: A, t: A): Boolean = gteq(t,s)
+
+  @inline final def compare(s: A, t: A): CMP_Result = {
+    if (s == t) CMP_EQ
+    else if (gt(s,t)) CMP_GT
+    else if (lt(s,t)) CMP_LT
     else CMP_NC
   }
-  def lt(x: A, y: A): Boolean = lteq(x,y) && !equiv(x,y)
-  def lteq(x: A, y: A): Boolean
-  def gt(x: A, y: A): Boolean = gteq(x,y) && !equiv(x,y)
-  def gteq(x: A, y: A): Boolean = lteq(y,x)
+  @inline final def canCompare(s: A, t: A): Boolean = compare(s,t) != CMP_NC
 }
 
-/** Trait for data ordered by a ordering-like relation.
-  * @see [[OrderingLike]]
-  */
-trait LikeOrdered[A] {
-  /** Result of comparing `this` with operand `that`. */
-  def compareTo(that: A): CompareResult
-
-  /** Returns true iff `this` and `that`  are comparable w.r.t. the underlying ordering-like relation. */
-  def comCompareTo(that: A): Boolean = Orderings.isComparable(compareTo(that))
-
-  /** Returns true iff (this compareTo that) < 0, i.e. if `this` is strictly smaller than `that`. */
-  def <  (that: A): Boolean = (this compareTo that) == CMP_LT
-  /** Returns true iff (this compareTo that) <= 0, i.e. if `this` is smaller than (or equal to) `that`. */
-  def <= (that: A): Boolean = Orderings.isLE(this compareTo that)
-  /** Returns true iff (this compareTo that) > 0, i.e. if `this` is strictly larger than `that`. */
-  def >  (that: A): Boolean = (this compareTo that) == CMP_GT
-  /** Returns true iff (this compareTo that) >= 0, i.e. if `this` is larger than (or equal to) `that`. */
-  def >= (that: A): Boolean = Orderings.isGE(this compareTo that)
-}
-
-/**
- * A trait for representing quasi-orderings.
- * A quasi-ordering is a
- *
- * - reflexive
- * - transitive
- *
- * binary relation.
- */
-trait QuasiOrdering[A] {
-  /** Result of comparing `x` with operand `y`.
-    *
-    * Returns `Some(res)` where:
-    *   - `res < 0` when `x < y`
-    *   - `res == 0` when `x == y`
-    *   - `res > 0` when  `x > y`
-    *
-    * Returns `None` if the objects cannot be compared.
-    * This is due to the fact that this object represents a partial ordering;
-    * it is possible that neither `compare(x,y) < 0` or `compare(y,x) < 0`
-    * holds for `x != y`.
-    */
-  def compare(x: A, y: A): Option[Int]
-
-  /** Returns `true` iff `x` and `y` are comparable w.r.t. the underlying ordering. */
-  def canCompare(x: A, y: A): Boolean = if (x == y) true
-                                        else compare(x,y).isDefined|| compare(y,x).isDefined
-
-  /** Strict comparison `x < y` w.r.t. the underlying ordering. */
-  def lt (x: A, y: A): Boolean = compare(x,y).getOrElse(Int.MaxValue) < 0
-  /** Comparison `x <= y` w.r.t. the underlying ordering. */
-  def lteq (x: A, y: A): Boolean = compare(x,y).getOrElse(Int.MaxValue) <= 0
-  /** Strict comparison `x > y` w.r.t. the underlying ordering. */
-  def gt (x: A, y: A): Boolean = compare(x,y).getOrElse(Int.MinValue) > 0
-  /** Comparison `x >= y` w.r.t. the underlying ordering. */
-  def gteq (x: A, y: A): Boolean = compare(x,y).getOrElse(Int.MinValue) >= 0
-  /** Comparison `x = y` w.r.t. the underlying ordering .*/
-  def eq (x: A, y: A): Boolean = compare(x,y).getOrElse(-4711) == 0
-}
-
-/** Trait for quasi-ordered data.
-  * @see [[QuasiOrdering]]
-  */
-trait QuasiOrdered[A] {
-  /** Result of comparing `this` with operand `that`.
-    *
-    * Returns `Some(res)` where:
-    *   - `res < 0` when `this < that`
-    *   - `res == 0` when `this == that`
-    *   - `res > 0` when  `this > that`
-    *
-    * Returns `None` if the objects cannot be compared.
-    * This is due to the fact that this object represents a partial ordering;
-    * it is possible that neither `this compareTo that < 0` or `that compareTo this < 0`
-    * holds for `this != that`.
-    */
-  def compareTo(that: A): Option[Int]
-
-  /** Returns true iff `this` and `that`  are comparable w.r.t. the underlying quasi-ordering. */
-  def comCompareTo(that: A): Boolean = compareTo(that).isDefined
-
-  /** Returns true iff (this compareTo that) < 0, i.e. if `this` is strictly smaller than `that`. */
-  def <  (that: A): Boolean = (this compareTo that).getOrElse(Int.MaxValue) < 0
-  /** Returns true iff (this compareTo that) <= 0, i.e. if `this` is smaller than (or equal to) `that`. */
-  def <= (that: A): Boolean = (this compareTo that).getOrElse(Int.MaxValue) <= 0
-  /** Returns true iff (this compareTo that) > 0, i.e. if `this` is strictly larger than `that`. */
-  def >  (that: A): Boolean = (this compareTo that).getOrElse(Int.MinValue) > 0
-  /** Returns true iff (this compareTo that) >= 0, i.e. if `this` is larger than (or equal to) `that`. */
-  def >= (that: A): Boolean = (this compareTo that).getOrElse(Int.MinValue) >= 0
-}
