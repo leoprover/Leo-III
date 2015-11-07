78267b8b76d381818e030a641f306e37e5c0ae5d
Alex <a.steen@fu-berlin.de>
2015-11-07 23:27:22 +0100

    updated mayunify method, added fusion of clauses with lifting of variables

--- src/main/scala/leo/modules/calculus/package.scala
+++ src/main/scala/leo/modules/calculus/package.scala
@@ -81,35 +81,41 @@ package object calculus {
 
   protected def mayUnify0(s: Term, t: Term, depth: Int): Boolean = {
     if (s == t) return true
-    if (s.ty == Signature.get.o && t.ty == Signature.get.o) return true
-    if (s.freeVars.isEmpty && t.freeVars.isEmpty) return false // contains to vars, cannot be unifiable
-    if (depth <= 0) return true
     if (s.ty != t.ty) return false
+//    if (s.ty == Signature.get.o && t.ty == Signature.get.o) return true
+    if (s.freeVars.isEmpty && t.freeVars.isEmpty) return false // contains to vars, cannot be unifiable TODO: Is this right?
+    if (depth <= 0) return true
     if (s.headSymbol.ty != t.headSymbol.ty) return false
-
 
     // Match case on head symbols:
     // flex-flex always works*, flex-rigid also works*, rigid-rigid only in same symbols
     // * = if same type
-    if (!s.headSymbol.isVariable && !t.headSymbol.isVariable) {
-      //        println("rigid-rigid case")
-      import leo.datastructures.Term._
-      // rigid-rigid
-      (s,t) match {
-        case (Symbol(id1), Symbol(id2)) => id1 == id2
-        case (Symbol(_), _) => false
-        case (_, Symbol(_)) => false
-        case (f1 ∙ args1, f2 ∙ args2) if args1.length > 0 && args2.length > 0 => mayUnify0(f1, f2, depth -1) && args1.zip(args2).forall{_ match {
-          case (Left(t1), Left(t2)) => mayUnify0(t1, t2, depth -1)
-          case (Right(ty1), Right(ty2)) => ty1 == ty2
-          case _ => false
-        } } // TODO: Do we need the first part? f1, f2 should be atoms
-        case (_ :::> body1, _ :::> body2) => mayUnify0(body1, body2, depth)
+    import leo.datastructures.Term._
+    // rigid-rigid
+    (s,t) match {
+      case (Symbol(id1), Symbol(id2)) => id1 == id2
+      case (Bound(_,_), _) => true
+      case (_, Bound(_,_)) => true
+      case (_ :::> body1, _ :::> body2) => mayUnify0(body1, body2, depth)
+      case (TypeLambda(s2), TypeLambda(t2)) => mayUnify0(s2, t2, depth)
+      case (f1 ∙ args1, f2 ∙ args2) if args1.length == args2.length => mayUnify0(f1, f2, depth -1) && args1.zip(args2).forall{_ match {
+        case (Left(t1), Left(t2)) => mayUnify0(t1, t2, depth -1)
+        case (Right(ty1), Right(ty2)) => ty1 == ty2
         case _ => false
-      }
-    } else {
-      true
+      } }
+      case _ => false
     }
+  }
+
+  import leo.datastructures.{Literal, Clause, Subst}
+  final def fuseLiterals(noLift: Seq[Literal], fv_noLift: Seq[(Int, Type)], lift: Seq[Literal], fv_lift: Seq[(Int, Type)]): Seq[Literal] = {
+    val subst = Subst.shift(fv_noLift.headOption.map(_._1).getOrElse(0))
+    (noLift ++ lift.map(_.substitute(subst)))
+  }
+  final def fuseLiteralsByFVCount(cl1: Seq[Literal], fv_cl1: Seq[(Int, Type)], cl2: Seq[Literal], fv_cl2: Seq[(Int, Type)]): Seq[Literal] = {
+    val (lift,nolift, liftBy) = if (fv_cl1.size > fv_cl2.size) (cl2, cl1, fv_cl2.headOption.map(_._1).getOrElse(0)) else (cl1, cl2, fv_cl1.headOption.map(_._1).getOrElse(0))
+    val subst = Subst.shift(liftBy)
+    (nolift ++ lift.map(_.substitute(subst)))
   }
 
 }
