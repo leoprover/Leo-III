b35cfc26e02d0a23e1776835124b1873e624f418
Alexander Steen <a.steen@fu-berlin.de>
2016-02-17 15:06:25 +0100

    found a library function that replaces the isSuffixOf method in unification

--- src/main/scala/leo/modules/calculus/Unification.scala
+++ src/main/scala/leo/modules/calculus/Unification.scala
@@ -262,18 +262,6 @@ object HuetsPreUnification extends Unification {
     * TODO: Alex: I filtered out all of those bound vars that have non-compatible type. Is that correct?
    */
   object ProjectRule extends HuetsRule[Seq[UEq]] {
-    final private def isSuffixOf(potentialSuffix: Seq[Type], searchIn: Seq[Type]): Boolean = {
-      if (potentialSuffix == searchIn) true
-      else {
-        if (potentialSuffix.size >= searchIn.size) false
-        else {
-          val searchIn2 = searchIn.drop(potentialSuffix.size)
-          if (searchIn2 == potentialSuffix) true
-          else false
-        }
-      }
-    }
-
     def apply(vargen: FreshVarGen, e: UEq): Seq[UEq] = {
       leo.Out.trace(s"Apply Project")
       // orienting the equation
@@ -281,7 +269,7 @@ object HuetsPreUnification extends Unification {
       val bvars = t.headSymbol.ty.funParamTypes.zip(List.range(1,t.headSymbol.ty.arity+1)).map(p => Term.mkBound(p._1,p._2)) // TODO
       leo.Out.finest(s"BVars in Projectrule: ${bvars.map(_.pretty).mkString(",")}")
       //Take only those bound vars that are itself a type with result type == type of general binding
-      val funBVars = bvars.filter(bvar => isSuffixOf(bvar.ty.funParamTypesWithResultType,t.headSymbol.ty.funParamTypesWithResultType))
+      val funBVars = bvars.filter(bvar => t.headSymbol.ty.funParamTypesWithResultType.endsWith(bvar.ty.funParamTypesWithResultType))
       leo.Out.finest(s"compatible type BVars in Projectrule: ${funBVars.map(_.pretty).mkString(",")}")
       val res = funBVars.map(e => (t.headSymbol,partialBinding(vargen, t.headSymbol.ty, e)))
 
