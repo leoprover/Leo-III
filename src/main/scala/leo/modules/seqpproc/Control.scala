b89e22c1df82946fd1b8eca55be28f2fcc890bb9
Alex <a.steen@fu-berlin.de>
2016-02-28 08:42:30 +0100

    implemented control for eq factoring

--- src/main/scala/leo/modules/seqpproc/Control.scala
+++ src/main/scala/leo/modules/seqpproc/Control.scala
@@ -5,8 +5,8 @@ import leo.modules.seqpproc.controlStructures._
 
 /**
   * Facade object for various control methods of the seq. proof procedure.
-  * @see [[leo.modules.seqpproc.SeqPProc]]
   *
+  * @see [[leo.modules.seqpproc.SeqPProc]]
   * @author Alexander Steen <a.steen@fu-berlin.de>
   */
 object Control {
@@ -17,9 +17,10 @@ object Control {
 }
 
 /** Package collcetion control objects for inference rules.
+  *
   * @see [[leo.modules.calculus.CalculusRule]] */
 package inferenceControl {
-  import leo.datastructures._
+  import leo.datastructures.{Clause, Literal, Term, Position}
   import Literal.Side
 
   package object inferenceControl {
@@ -35,7 +36,7 @@ package inferenceControl {
     * @since 22.02.16
     */
   protected[modules] object ParamodControl {
-    import inferenceControl._
+//    import inferenceControl._
 
     final def paramodSet(cl: ClauseWrapper, withset: Set[ClauseWrapper]): Set[ClauseWrapper] = {
       var results: Set[ClauseWrapper] = Set()
@@ -101,7 +102,7 @@ package inferenceControl {
     ///////////////////////////////////////////////////////
 
     type Subterm = Term
-    type IntoConfiguration = (LiteralIndex, Literal, Side, Position, Subterm)
+    type IntoConfiguration = (inferenceControl.LiteralIndex, Literal, Side, Position, Subterm)
 
     final private def intoConfigurationIterator(cl: Clause): Iterator[IntoConfiguration] = new Iterator[IntoConfiguration] {
 
@@ -166,31 +167,51 @@ package inferenceControl {
   }
 
   protected[modules] object FactorizationControl {
-    import inferenceControl._
+
+    import leo.datastructures.Not
 
     final def factor(cl: ClauseWrapper): Set[ClauseWrapper] = {
+      Out.debug(s"Factor in ${cl.id}")
       var res: Set[ClauseWrapper] = Set()
       val clause = cl.cl
       val maxLitIt = new LiteralSideIterator(clause, true, true, true)
 
       while (maxLitIt.hasNext) {
-        // FIXME Only right of maxlit search
         val (maxLitIndex, maxLit, maxLitSide) = maxLitIt.next()
-        val otherLits: Seq[Literal] = ???
-        val factor = factorLit(cl, maxLitIndex, maxLit, maxLitSide, otherLits)
-        res = res union factor
+        val otherLitIt = new LiteralSideIterator(clause, false, true, true)
+
+        while (otherLitIt.hasNext) {
+          val (otherLitIndex, otherLit, otherLitSide) = otherLitIt.next()
+
+          if (maxLitIndex != otherLitIndex) {
+            val realMaxLit = if (maxLit.polarity)
+              maxLit
+            else {
+              assert(maxLit.flexHead)
+              Literal(Not(maxLit.left), true) // Is that right?
+            }
+            val realOtherLit = if (otherLit.polarity)
+              otherLit
+            else {
+              assert(otherLit.flexHead)
+              Literal(Not(otherLit.left), true)
+            }
+            val (maxLitMaxSide, maxLitOtherSide) = Literal.getSidesOrdered(realMaxLit, maxLitSide)
+            val (otherLitMaxSide, otherLitOtherSide) = Literal.getSidesOrdered(realOtherLit, otherLitSide)
+            val test1 = leo.modules.calculus.mayUnify(maxLitMaxSide, otherLitMaxSide)
+            val test2 = leo.modules.calculus.mayUnify(maxLitOtherSide, otherLitOtherSide)
+            Out.finest(s"Test unify ($test1): ${maxLitMaxSide.pretty} = ${otherLitMaxSide.pretty}")
+            Out.finest(s"Test unify ($test2): ${maxLitOtherSide.pretty} = ${otherLitOtherSide.pretty}")
+            if (test1 && test2) {
+              val factor = OrderedEqFac(clause, maxLitIndex, maxLitSide, otherLitIndex, otherLitSide)
+              val result = ClauseWrapper(factor, InferredFrom(OrderedEqFac, Set(cl)))
+              res = res + result
+            }
+          }
+        }
       }
 
-      res
-    }
-
-    final def factorLit(cl: ClauseWrapper,
-                        maxLitIndex: LiteralIndex, maxLit: Literal, maxLitSide: Side,
-                        others: Seq[Literal]): Set[ClauseWrapper] = {
-      var res: Set[ClauseWrapper] = Set()
-
-      ???
-
+      Out.trace(s"Factor result:\n\t${res.map(_.pretty).mkString("\n\t")}")
       res
     }
   }
