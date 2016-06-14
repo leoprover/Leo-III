3ea51107ca26acf645646239760af4e87a911321
Alex <a.steen@fu-berlin.de>
2016-06-14 21:31:03 +0200

    fixed leibniz eq replacement to correctly increase de bruijn indices

--- src/main/scala/leo/modules/calculus/NormalizationRules.scala
+++ src/main/scala/leo/modules/calculus/NormalizationRules.scala
@@ -206,7 +206,7 @@ object ReplaceLeibnizEq extends CalculusRule {
   }
 
   def apply(cl: Clause, bindings: Map[Int, Term]): (Clause, Subst) = {
-    val gbMap = bindings.mapValues(t => Term.mkTermAbs(t.ty, ===(t, Term.mkBound(t.ty, 1))))
+    val gbMap = bindings.mapValues(t => Term.mkTermAbs(t.ty, ===(t.substitute(Subst.shift(1)), Term.mkBound(t.ty, 1))))
     val subst = Subst.fromMap(gbMap)
     val newLits = cl.lits.map(_.substitute(subst))
     (Clause((newLits)), subst)
