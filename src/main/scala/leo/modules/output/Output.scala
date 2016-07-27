60abfde62b13a219683bb46c195cc012a5db0024
Alexander Steen <a.steen@fu-berlin.de>
2016-07-27 14:24:11 +0200

    some work on output module (not working yet)

--- src/main/scala/leo/modules/output/Output.scala
+++ src/main/scala/leo/modules/output/Output.scala
@@ -1,11 +1,13 @@
 package leo.modules.output
 
 /**
- * Created by lex on 07.11.14.
- */
-trait Output {
-  def output: String
-
-}
+  * Trait for wrapping strings. Might be handy if the computation of the strings
+  * are somewhat involved/complex and you just want to compute them if they are
+  * indeed needed.
+  *
+  * @author Alexander Steen <a.steen@fu-berlin.de>
+  * @since 07.11.14
+  */
+trait Output extends Function0[String]{}
 
 
