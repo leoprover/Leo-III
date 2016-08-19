5d0bccf4ed5f5416cfa70fb00229fda66b9e36fb
Hans-Jörg Schurr <commits@schurr.at>
2016-08-19 23:06:06 +0200

    More function prototypes.

--- src/main/scala/leo/modules/sat_solver/PicoSAT.scala
+++ src/main/scala/leo/modules/sat_solver/PicoSAT.scala
@@ -8,6 +8,8 @@ import ch.jodersky.jni.nativeLoader
   *
   * Objects of this class represent independent PicoSAT contexts. Since PicoSAT is an iterative solver the
   * contexts are stateful. After solving additional clauses can be added.
+  * Most of the methods of this object map directly to functions of the PicoSAT C API. See the picosat.h file
+  * for an exact documentation of the function behavior.
   *
   * @author Hans-Jörg Schurr
   * @since 7/27/16.
@@ -64,11 +66,97 @@ class PicoSAT private (enableTracing: Boolean) {
     addClause(lits)
   }
 
-  /* Solves the current context.
-   * @return The result of attempting to solve the context.
-   */
-  def solve() : State= {
+  /**
+    * Sets the default initial phase.
+    *
+    * Sets the assignment chosen for a variable initially.
+    * After a variable has been assigned the first time, it will always
+    * be assigned the previous value if it is picked as decision variable.
+    *
+    * @param phase  0 = false
+    *               1 = true
+    *               2 = Jeroslow-Wang (default)
+    *               3 = random initial phase
+    */
+  def setGlobalDefaultPhase(phase: Int) = {
+    picosat_set_global_default_phase(context, phase)
+  }
+
+  /**
+    * Sets the next or initial phase of a variable if picked as a decision variable.
+    *
+    * @param lit The literal.
+    * @param phase negative = next value if picked as decision variable is false
+    *              positive = next value if picked as decision variable is true
+    *              0        = use global default phase as next value and
+    *                         assume 'lit' was never assigned
+    */
+  def setDefaultPhase(lit: Int, phase: Int) = {
+    picosat_set_default_phase_lit(context, lit, phase)
+  }
+
+  /** Solves the current context.
+    * @return The result of attempting to solve the context.
+    */
+  def solve() : State = {
     State(picosat_sat(context, -1))
+  }
+
+  /**
+    * Resets all phases.
+    */
+  def resetPhases() = {
+    picosat_reset_phases(context)
+  }
+
+  /**
+    * Resets all scores.
+    *
+    * Resetting scores and phases does not delete learned clauses or move head tail pointers.
+    */
+  def resetScores() = {
+    picosat_reset_scores(context)
+  }
+
+  /**
+    * Deletes a given percentage of large learned clauses.
+    *
+    * If the current status  is SAT the assignment is reset first. If the given percentage is 100% all large learned
+    * clauses are deleted.
+    *
+    * @param percentage The percentage of clauses to delete.
+    */
+  def removeLearnedClauses(percentage: Int) = {
+    picosat_remove_learned(context, percentage)
+  }
+
+  /**
+    * Marks a variable as more important.
+    *
+    * Variables marked as more important are used as decision variables first.
+    *
+    * @param lit The variable to to mark as more important.
+    */
+  def setMoreImportant(lit: Int) = {
+    picosat_set_more_important_lit(context, lit)
+  }
+
+  /**
+    * Marks a variable as less important.
+    *
+    * Variables marked as less important are used as decision variables last.
+    *
+    * @param lit The variable to to mark as less important.
+    */
+  def setLessImportant(lit: Int) = {
+    picosat_set_less_important_lit(context, lit)
+  }
+
+  /**
+    * @return True if the CNF is unsatisfiable becose the empty clause was added or derived.
+    */
+  def inconsistent = {
+    picosat_inconsistent(context) != 0
   }
 
   override def finalize(): Unit = picosat_reset(context)
@@ -80,6 +168,14 @@ class PicoSAT private (enableTracing: Boolean) {
   @native def picosat_inc_max_var(context: Long) : Int
   @native def picosat_add(context: Long, lit: Int) : Int
   @native def picosat_sat(context: Long, decision_limit: Int) : Int
+  @native def picosat_set_global_default_phase(context: Long, phase: Int) : Unit
+  @native def picosat_set_default_phase_lit(context: Long, lit: Int, phase: Int) : Unit
+  @native def picosat_reset_phases(context: Long) : Unit
+  @native def picosat_reset_scores(context: Long) : Unit
+  @native def picosat_remove_learned(context: Long, percentage: Int) : Unit
+  @native def picosat_set_more_important_lit(context: Long, lit: Int) : Unit
+  @native def picosat_set_less_important_lit(context: Long, lit: Int) : Unit
+  @native def picosat_inconsistent(context: Long) : Int
 }
 
 /*
