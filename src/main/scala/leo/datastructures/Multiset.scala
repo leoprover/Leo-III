c935bf96621e60d09ef8f40597c3235f9f356fac
Alexander Steen <a.steen@fu-berlin.de>
2016-11-09 12:01:20 +0100

    update docu of multiset

--- src/main/scala/leo/datastructures/Multiset.scala
+++ src/main/scala/leo/datastructures/Multiset.scala
@@ -1,29 +1,45 @@
 package leo.datastructures
 
 /**
-  * Created by lex on 11/9/16.
+  * Multisets.
+  *
+  * @tparam A The type of the elements in the multiset
+  * @author Alexander Steen <a.steen@fu-berlin.de>
   */
 trait Multiset[A]
   extends Iterable[A] {
 
+  /** Returns an iterator over the distinct elements of this multiset. */
   def distinctIterator: Iterator[A]
 
+  /** Returns true iff `elem` is contained within the multiset. */
   def contains(elem: A): Boolean
 
+  /** Returns a new multiset that contains all elements of `this` plus `elem`, */
   def +(elem: A): Multiset[A] = this.+(elem,1)
+  /** Returns a new multiset that contains all elements of `this` plus `count` times `elem`, */
   def +(elem: A, count: Int): Multiset[A]
 
+  /** Returns a new multiset that contains all elements of `this` without `elem`, */
   def -(elem: A): Multiset[A]
 
+  /** The number of occurrences of `elem`. */
   def multiplicity(elem: A): Int
 
+  /** Returns a set of each distinct element. */
   def distinct: Set[A]
+  /** The size of the multiset when regarded as a set. */
   def distinctSize: Int
 
+  /** Returns a new multiset that is the intersection of `this` and `that`. */
   def intersect(that: Multiset[A]): Multiset[A]
+  /** Returns a new multiset that is the sum of `this` and `that`. */
   def sum(that: Multiset[A]): Multiset[A]
+  /** Returns a new multiset that is the union of `this` and `that`. Note that
+    * this is NOT the sum (i.e. it contains the maximal number of element occurences on both
+    * multisets, not the sum of both). */
   def union(that: Multiset[A]): Multiset[A]
-
+  /** Returns true off `this` is a subset of `that`. */
   def subset(that:  Multiset[A]): Boolean = {
     distinctIterator.forall(elem =>
       multiplicity(elem) <= that.multiplicity(elem)
