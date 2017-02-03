feb46c55ae696e6118e6fc3d9e14a1e08e15d433
Alex <a.steen@fu-berlin.de>
2017-02-03 20:05:34 +0100

    some new orderings

--- src/main/scala/leo/datastructures/impl/orderings/ClauseProxyOrderings.scala
+++ src/main/scala/leo/datastructures/impl/orderings/ClauseProxyOrderings.scala
@@ -20,3 +20,42 @@ import leo.datastructures.ClauseProxy
     def compare(a: ClauseProxy, b: ClauseProxy) = implicitly[Ordering[Tuple2[Double, Int]]].compare((1 - ((1+a.cl.posLits.size)/(1+a.cl.lits.size)), a.weight), (1 - ((1+b.cl.posLits.size)/(b.cl.lits.size+1)), b.weight))
   }
 
+
+object CPO_OldestFirst extends ClauseProxyOrdering {
+  final def compare(a: ClauseProxy, b: ClauseProxy) = {
+    val aAge = a.id; val bAge = b.id
+    aAge.compareTo(bAge)
+  }
+}
+
+object CPO_GoalsFirst2 extends ClauseProxyOrdering {
+  def compare(a: ClauseProxy, b: ClauseProxy) = {
+    val aGoalRatio: Float = if (a.cl.lits.isEmpty) 1 else a.cl.negLits.size.toFloat/a.cl.lits.size.toFloat
+    val bGoalRatio: Float = if (b.cl.lits.isEmpty) 1 else b.cl.negLits.size.toFloat/b.cl.lits.size.toFloat
+    aGoalRatio.compareTo(bGoalRatio)
+  }
+}
+
+object CPO_NonGoalsFirst2 extends ClauseProxyOrdering {
+  def compare(a: ClauseProxy, b: ClauseProxy) = {
+    val aGoalRatio: Float = if (a.cl.lits.isEmpty) 1 else a.cl.posLits.size.toFloat/a.cl.lits.size.toFloat
+    val bGoalRatio: Float = if (b.cl.lits.isEmpty) 1 else b.cl.posLits.size.toFloat/b.cl.lits.size.toFloat
+    aGoalRatio.compareTo(bGoalRatio)
+  }
+}
+
+object CPO_SOSFirst extends ClauseProxyOrdering {
+  final def compare(a: ClauseProxy, b: ClauseProxy) = {
+    import leo.datastructures.isPropSet
+    import leo.datastructures.ClauseAnnotation.PropSOS
+    val aProp = a.properties; val bProp = b.properties
+    if (isPropSet(PropSOS, aProp)) {
+      if (isPropSet(PropSOS, bProp)) 0
+      else 1
+    } else {
+      if (isPropSet(PropSOS, bProp)) -1
+      else 0
+    }
+  }
+}
+
