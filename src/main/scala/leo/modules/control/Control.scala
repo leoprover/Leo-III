849265deb760dcbec20ed726df77333ea267de44
Alexander Steen <a.steen@fu-berlin.de>
2017-02-13 13:42:06 +0100

    refactored paramod control

--- src/main/scala/leo/modules/control/Control.scala
+++ src/main/scala/leo/modules/control/Control.scala
@@ -184,7 +184,7 @@ package inferenceControl {
       val withConfigurationIt = new LiteralSideIterator(withClause, true, true, false)(sig)
       while (withConfigurationIt.hasNext) {
         val (withIndex, withLit, withSide) = withConfigurationIt.next()
-        val withTerm = if (withSide) withLit.left else withLit.right
+        val withTerm = Literal.selectSide(withLit, withSide)
 
         assert(withClause.lits(withIndex) == withLit, s"$withIndex in ${withClause.pretty(sig)}\n lit = ${withLit.pretty(sig)}")
         assert(withLit.polarity)
@@ -192,107 +192,84 @@ package inferenceControl {
         val intoConfigurationIt = intoConfigurationIterator(intoClause)(sig)
         while (intoConfigurationIt.hasNext) {
           val (intoIndex, intoLit, intoSide, intoPos, intoTerm) = intoConfigurationIt.next()
-          leo.Out.finest(s"check with ${withTerm.pretty(sig)}, into: ${intoTerm.pretty(sig)}: ${leo.modules.calculus.mayUnify(withTerm, intoTerm)}")
           assert(!intoLit.flexflex)
           if (intoPos == Position.root &&
             ((intoWrapper.id == withWrapper.id && intoIndex == withIndex) ||
               (!withLit.equational && !intoLit.equational && intoLit.polarity))) {
             /* skip, this generates a redundant clause */
           } else {
-            if (leo.modules.calculus.mayUnify(withTerm, intoTerm)) {
-              if (!intoTerm.isVariable) {
-                Out.trace(s"May unify: ${withTerm.pretty(sig)} with ${intoTerm.pretty(sig)} (subterm at ${intoPos.pretty})")
-                Out.finest(s"with: ${withClause.pretty(sig)}")
-                Out.finest(s"withside: ${withSide.toString}")
-                Out.finest(s"into: ${intoClause.pretty(sig)}")
-                Out.finest(s"intoside: ${intoSide.toString}")
-                // We shift all lits from intoClause to make the universally quantified variables distinct from those of withClause.
-                // We cannot use _.substitute on literal since this will forget the ordering
-                val termShift = Subst.shift(withClause.maxImplicitlyBound)
-                val typeShift = Subst.shift(withClause.maxTypeVar)
-                val shiftedIntoClause: Clause = Clause(
-                  intoClause.lits.map {l =>
-                    if (l.equational) {
-                      // since we only lift variables, the orientation property isnt affected
-                      // we may safely assume that the new lits are also orientable if the original ones were orientable.
-                      Literal.mkLit(l.left.substitute(termShift, typeShift), l.right.substitute(termShift, typeShift), l.polarity, l.oriented)
-                    } else {
-                      Literal.mkLit(l.left.substitute(termShift, typeShift), l.polarity)
-                    }
-                  }
-                )
-                val shiftedIntoTerm: Term = intoTerm.substitute(Subst.shift(withClause.maxImplicitlyBound-intoPos.abstractionCount), typeShift)
-                Out.finest(s"shifted into: ${shiftedIntoClause.pretty(sig)}")
-                Out.finest(s"shiftedIntoSubterm: ${shiftedIntoTerm.pretty(sig)}")
-
-                val newCl = if (withTerm.ty == shiftedIntoTerm.ty) {
-                  // No type unification needed at this point
-                  OrderedParamod(withClause, withIndex, withSide,
-                    shiftedIntoClause, intoIndex, intoSide, intoPos, shiftedIntoTerm)(sig)
-                } else {
-                  import leo.modules.calculus.{HuetsPreUnification => Unification}
-                  leo.Out.finest(s"Nonequal type, calculating type unification ...")
-                  leo.Out.finest(s"withTerm.ty: ${withTerm.ty.pretty(sig)}")
-                  leo.Out.finest(s"intoTerm.ty: ${shiftedIntoTerm.ty.pretty(sig)}")
-                  val tyUniResult = Unification.unify(withTerm.ty, shiftedIntoTerm.ty)
-                  if (tyUniResult.isDefined) {
-                    leo.Out.finest(s"Type unification succeeded.")
-                    val tySubst = tyUniResult.get
-                    leo.Out.finest(s"Type subst: ${tySubst.pretty}.")
-                    // FIXME the clauses below are not ordered anymore. This needs to be fixed.
-                    val substWithClause = withClause.substitute(Subst.id, tySubst)
-                    val substIntoClause = shiftedIntoClause.substitute(Subst.id, tySubst)
-                    val substIntoTerm = shiftedIntoTerm.substitute(Subst.id, tySubst)
-                    OrderedParamod(substWithClause, withIndex, withSide,
-                      substIntoClause, intoIndex, intoSide, intoPos, substIntoTerm)(sig)
+            val shouldParamod0 = shouldParamod(withTerm, intoTerm)
+            leo.Out.finest(s"shouldParamod: $shouldParamod0\n\twith ${withTerm.pretty(sig)}\n\tinto: ${intoTerm.pretty(sig)}")
+            if (!intoTerm.isVariable && shouldParamod0) {
+              Out.trace(s"May unify: ${withTerm.pretty(sig)} with ${intoTerm.pretty(sig)} (subterm at ${intoPos.pretty})")
+              Out.finest(s"with: ${withClause.pretty(sig)}")
+              Out.finest(s"withside: ${withSide.toString}")
+              Out.finest(s"into: ${intoClause.pretty(sig)}")
+              Out.finest(s"intoside: ${intoSide.toString}")
+              // We shift all lits from intoClause to make the universally quantified variables distinct from those of withClause.
+              // We cannot use _.substitute on literal since this will forget the ordering
+              val termShift = Subst.shift(withClause.maxImplicitlyBound)
+              val typeShift = Subst.shift(withClause.maxTypeVar)
+              val shiftedIntoClause: Clause = Clause(
+                intoClause.lits.map {l =>
+                  if (l.equational) {
+                    // since we only lift variables, the orientation property isnt affected
+                    // we may safely assume that the new lits are also orientable if the original ones were orientable.
+                    Literal.mkLit(l.left.substitute(termShift, typeShift), l.right.substitute(termShift, typeShift), l.polarity, l.oriented)
                   } else {
-                    leo.Out.finest(s"Type unification failed.")
-                    null
+                    Literal.mkLit(l.left.substitute(termShift, typeShift), l.polarity)
                   }
                 }
-                if (newCl != null) {
-                  val newProperties = if (isPropSet(ClauseAnnotation.PropSOS, withWrapper.properties) || isPropSet(ClauseAnnotation.PropSOS, intoWrapper.properties))
-                    ClauseAnnotation.PropNeedsUnification |  ClauseAnnotation.PropSOS
-                  else ClauseAnnotation.PropNeedsUnification
-                  val newClWrapper = AnnotatedClause(newCl, InferredFrom(OrderedParamod, Seq(withWrapper, intoWrapper)), newProperties)
-                  Out.finest(s"Result: ${newClWrapper.pretty(sig)}")
-                  results = results + newClWrapper
+              )
+              val shiftedIntoTerm: Term = intoTerm.substitute(Subst.shift(withClause.maxImplicitlyBound-intoPos.abstractionCount), typeShift)
+              Out.finest(s"shifted into: ${shiftedIntoClause.pretty(sig)}")
+              Out.finest(s"shiftedIntoSubterm: ${shiftedIntoTerm.pretty(sig)}")
+
+              val newCl = if (withTerm.ty == shiftedIntoTerm.ty) {
+                // No type unification needed at this point
+                OrderedParamod(withClause, withIndex, withSide,
+                  shiftedIntoClause, intoIndex, intoSide, intoPos, shiftedIntoTerm)(sig)
+              } else {
+                import leo.modules.calculus.{HuetsPreUnification => Unification}
+                leo.Out.finest(s"Nonequal type, calculating type unification ...")
+                leo.Out.finest(s"withTerm.ty: ${withTerm.ty.pretty(sig)}")
+                leo.Out.finest(s"intoTerm.ty: ${shiftedIntoTerm.ty.pretty(sig)}")
+                val tyUniResult = Unification.unify(withTerm.ty, shiftedIntoTerm.ty)
+                if (tyUniResult.isDefined) {
+                  leo.Out.finest(s"Type unification succeeded.")
+                  val tySubst = tyUniResult.get
+                  leo.Out.finest(s"Type subst: ${tySubst.pretty}.")
+                  // FIXME the clauses below are not ordered anymore. This needs to be fixed.
+                  val substWithClause = withClause.substitute(Subst.id, tySubst)
+                  val substIntoClause = shiftedIntoClause.substitute(Subst.id, tySubst)
+                  val substIntoTerm = shiftedIntoTerm.substitute(Subst.id, tySubst)
+                  OrderedParamod(substWithClause, withIndex, withSide,
+                    substIntoClause, intoIndex, intoSide, intoPos, substIntoTerm)(sig)
+                } else {
+                  leo.Out.finest(s"Type unification failed.")
+                  null
                 }
               }
-            } else {
-              if (!withLit.equational && !intoLit.equational && !intoLit.polarity && intoPos == Position.root) {
-                // implicitly: intoWrapper.id != withWrapper.id since withLit.polarity == true
-                if (withTerm.headSymbol == intoTerm.headSymbol) {
-                  // this means we can do at least a one-step simplication (Decomp) during unification
-                  Out.trace(s"Simulated Resolution step")
-                  val termShift = Subst.shift(withClause.maxImplicitlyBound)
-                  val typeShift = Subst.shift(withClause.maxTypeVar)
-                  val shiftedIntoClause: Clause = Clause(
-                    intoClause.lits.map {l =>
-                      if (l.equational) {
-                        // since we only lift variables, the orientation property isnt affected
-                        // we may safely assume that the new lits are also orientable if the original ones were orientable.
-                        Literal.mkLit(l.left.substitute(termShift, typeShift), l.right.substitute(termShift, typeShift), l.polarity, l.oriented)
-                      } else {
-                        Literal.mkLit(l.left.substitute(termShift, typeShift), l.polarity)
-                      }
-                    }
-                  )
-                  Out.finest(s"shifted into: ${shiftedIntoClause.pretty(sig)}")
-                  import leo.modules.HOLSignature.LitTrue
-                  val newCl = OrderedParamod(withClause, withIndex, !withSide,
-                    shiftedIntoClause, intoIndex, !intoSide, intoPos, LitTrue(), true)(sig)
-
-                  val newClWrapper = AnnotatedClause(newCl, InferredFrom(OrderedParamod, Seq(withWrapper, intoWrapper)), ClauseAnnotation.PropSOS | ClauseAnnotation.PropNeedsUnification)
-                  Out.finest(s"Result: ${newClWrapper.pretty(sig)}")
-                  results = results + newClWrapper
-                }
+              if (newCl != null) { // May be null if type unification was not successful
+                val newProperties = if (isPropSet(ClauseAnnotation.PropSOS, withWrapper.properties) || isPropSet(ClauseAnnotation.PropSOS, intoWrapper.properties))
+                  ClauseAnnotation.PropNeedsUnification |  ClauseAnnotation.PropSOS
+                else ClauseAnnotation.PropNeedsUnification
+                val newClWrapper = AnnotatedClause(newCl, InferredFrom(OrderedParamod, Seq(withWrapper, intoWrapper)), newProperties)
+                Out.finest(s"Result: ${newClWrapper.pretty(sig)}")
+                results = results + newClWrapper
               }
             }
           }
         }
       }
       results
+    }
+    /** We should paramod if either the terms are unifiable or if at least one unification rule step can be executed. */
+    private final def shouldParamod(withTerm: Term, intoTerm: Term): Boolean = {
+      val withHd = withTerm.headSymbol
+      val intoHd = intoTerm.headSymbol
+      if (withHd == intoHd && withHd.isConstant) true
+      else leo.modules.calculus.mayUnify(withTerm, intoTerm)
     }
 
     ////////////////////////////////////////////////////////
