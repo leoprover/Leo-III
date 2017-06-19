6d14196c5a1521e8810f314a7121c912bdec533c
Alexander Steen <a.steen@fu-berlin.de>
2017-06-19 10:24:08 +0200

    added correct translation of choice to THF

--- src/main/scala/leo/modules/output/ToTPTP.scala
+++ src/main/scala/leo/modules/output/ToTPTP.scala
@@ -4,7 +4,7 @@ import leo.datastructures._
 import Term._
 import leo.datastructures.Type._
 import leo.datastructures._
-import leo.modules.HOLSignature.{LitFalse, |||, &, Not, Forall, Exists, TyForall, !===, ===, Impl, <=, <=>, ~&, ~|||, <~>}
+import leo.modules.HOLSignature.{LitFalse, |||, &, Not, Forall, Exists, TyForall, Choice, !===, ===, Impl, <=, <=>, ~&, ~|||, <~>}
 import leo.modules.SZSException
 
 import scala.annotation.tailrec
@@ -336,6 +336,12 @@ object ToTPTP {
         }
       case TyForall(_) => val (tyAbsCount, body) = collectTyForall(t)
         s"! [${(1 to tyAbsCount).map(i => "T" + intToName(i - 1) + ": $tType").mkString(",")}]: (${toTPTP0(body, tyVarCount+tyAbsCount, bVars)(sig)})"
+      case Choice(_) => val (bVarTys, body) = collectChoice(t)
+                        val newBVars = makeBVarList(bVarTys, bVars.size)
+        body match {
+          case Forall(_) | Exists(_) | Not(_) => s"${sig(Choice.key).name} [${newBVars.map({case (s,ty) => s"$s:${typeToTHF0(ty,tyVarCount)(sig)}"}).mkString(",")}]: ${toTPTP0(body, tyVarCount, fusebVarListwithMap(newBVars,bVars))(sig)}"
+          case _ => s"${sig(Choice.key).name} [${newBVars.map({case (s,ty) => s"$s:${typeToTHF0(ty, tyVarCount)(sig)}"}).mkString(",")}]: (${toTPTP0(body, tyVarCount, fusebVarListwithMap(newBVars,bVars))(sig)})"
+        }
       // Binary connectives
       case t1 ||| t2 => t1 match {
         case _ ||| _| Not(_) | Forall(_) | Exists(_) => t2 match {
@@ -475,7 +481,20 @@ object ToTPTP {
   @inline final private def collectExists0(vars: Seq[Type], t: Term): (Seq[Type], Term) = {
     t match {
       case Exists(ty :::> b) => collectExists0(vars :+ ty, b)
-      case Exists(_) => throw new IllegalArgumentException("Unexcepted body term in existsl quantification decomposition.")
+      case Exists(_) => throw new IllegalArgumentException("Unexcepted body term in exists quantification decomposition.")
+      case _ => (vars, t)
+    }
+  }
+
+  /** Gather consecutive all-quantifications (nameless). */
+  final private def collectChoice(t: Term): (Seq[Type], Term) = {
+    collectChoice0(Seq.empty, t)
+  }
+  @tailrec
+  @inline final private def collectChoice0(vars: Seq[Type], t: Term): (Seq[Type], Term) = {
+    t match {
+      case Choice(ty :::> b) => collectChoice0(vars :+ ty, b)
+      case Choice(_) => throw new IllegalArgumentException("Unexcepted body term in choice decomposition.")
       case _ => (vars, t)
     }
   }
