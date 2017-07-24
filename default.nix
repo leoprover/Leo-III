37f66c150cd467d1d41d5036a236de945f1067dc
Alexander Steen <a.steen@fu-berlin.de>
2017-07-24 14:56:48 +0200

    removed static TreeLimitedRun compile

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 
 stdenv.mkDerivation rec {
   name = "Leo-III";
-  buildInputs = [stdenv openjdk cmake gcc glibc.static sbt];
+  buildInputs = [stdenv openjdk cmake gcc sbt];
   JAVA_HOME = "${openjdk}/lib/openjdk/";
   shellHook = ''
       export _JAVA_OPTIONS=-Duser.home=$HOME
