14d99471fd91deb96cd446bd41667755f7f489d6
Alexander Steen <a.steen@fu-berlin.de>
2020-11-26 10:32:48 +0100

    some work on tokenizer

--- src/main/scala/leo/modules/parsers/TPTPKloeppelParser.scala
+++ src/main/scala/leo/modules/parsers/TPTPKloeppelParser.scala
@@ -34,9 +34,16 @@ object TPTPKloeppelParser {
     private[this] var curLine: Int = 1
     private[this] var curOffset: Int = 1
 
+    private[this] var curItem: TPTPLexer.TPTPLexerToken = _
+
     @inline private[this] def line(): Unit = { curLine += 1; curOffset = 1 }
     @inline private[this] def step(): Unit = { curOffset += 1 }
-    @inline private[this] def consume(): Unit = { iter.next(); step() }
+    @inline private[this] def consume(): Char = { val res = iter.next(); step(); res }
+    @inline private[this] def isLowerAlpha(ch: Char): Boolean = ch.isLower && ch <= 'z' // only select ASCII
+    @inline private[this] def isUpperAlpha(ch: Char): Boolean = ch.isUpper && ch <= 'Z' // only select ASCII
+    @inline private[this] def isAlpha(ch: Char): Boolean = isLowerAlpha(ch) || isUpperAlpha(ch)
+    @inline private[this] def isNumeric(ch: Char): Boolean = ch.isDigit && ch <= '9' // only select ASCII
+    @inline private[this] def isAlphaNumeric(ch: Char): Boolean = isAlpha(ch) || isNumeric(ch) || ch == '_'
 
     @tailrec
     override def hasNext: Boolean = iter.hasNext && {
@@ -102,48 +109,144 @@ object TPTPKloeppelParser {
     }
 
     override def next(): TPTPLexer.TPTPLexerToken = {
+      if (curItem == null) {
+        getNextToken
+      } else {
+        val result = curItem
+        curItem = null
+        result
+      }
+    }
+
+    def peek(): TPTPLexer.TPTPLexerToken = {
+      if (curItem == null) {
+        curItem = getNextToken
+        curItem
+      } else {
+        curItem
+      }
+    }
+
+    private[this] def getNextToken: TPTPLexer.TPTPLexerToken = {
       import TPTPLexer.TPTPLexerTokenType._
 
       if (!hasNext) throw new NoSuchElementException // also to remove ignored input such as comments etc.
       else {
-        val ch = iter.head
+        val ch = consume()
         // BIG switch case over all different possibilities.
         ch match {
           // most frequent tokens
-          case '(' => LPAREN
-          case ')' => RPAREN
-          case '[' => LBRACKET
-          case ']' => RBRACKET
-          case _ if ch.isLower && ch <= 'z' => ??? // lower word
-          case _ if ch.isUpper && ch <= 'Z' => ??? // upper word
-          case ',' => COMMA
-          case '$' => ??? // doller word or doller doller word
-          case ':' => ??? // COLON or Assignment
+          case '(' => tok(LPAREN, 1)
+          case ')' => tok(RPAREN, 1)
+          case '[' => tok(LBRACKET, 1)
+          case ']' => tok(RBRACKET, 1)
+          case _ if isLowerAlpha(ch) => // lower word
+            val offset = curOffset-1
+            val payload = collectAlphaNums(ch)
+            (LOWERWORD, payload, curLine, offset)
+          case _ if isUpperAlpha(ch) && ch <= 'Z' => // upper word
+            val offset = curOffset-1
+            val payload = collectAlphaNums(ch)
+            (UPPERWORD, payload, curLine, offset)
+          case ',' => tok(COMMA, 1)
+          case '$' =>  // doller word or doller doller word
+            val offset = curOffset-1
+            if (iter.hasNext) {
+              if (iter.head == '$') { // DollarDollarWord
+                consume()
+                if (iter.hasNext && isAlphaNumeric(iter.head)) {
+                  val payload = collectAlphaNums(ch)
+                  (DOLLARDOLLARWORD, "$" ++ payload, curLine, offset)
+                } else {
+                  throw new TPTPParseException(s"Unrecognized token: Invalid or empty DollarDollarWord)", curLine, offset)
+                }
+              } else if (isAlphaNumeric(iter.head)) {
+                val payload = collectAlphaNums(ch)
+                (DOLLARWORD, payload, curLine, offset)
+              } else
+                throw new TPTPParseException(s"Unrecognized token '$$${iter.head}' (invalid dollar word)", curLine, offset)
+            } else {
+              throw new TPTPParseException("Unrecognized token '$' (empty dollar word)", curLine, offset)
+            }
+          case ':' => // COLON or Assignment
+            if (iter.hasNext && iter.head == '=') {
+              consume()
+              tok(ASSIGNMENT, 2)
+            } else
+              tok(COLON, 1)
           // connectives
-          case '|' => OR
-          case '&' => AND
-          case '^' => LAMBDA
-          case '<' => ??? // IFF, NIFF, IF, but also subtype
-          case '=' => ??? // IMPL or EQUALS
-          case '~' => ??? // NOT, NAND, or NOR
+          case '|' => tok(OR, 1)
+          case '&' => tok(AND, 1)
+          case '^' => tok(LAMBDA, 1)
+          case '<' => // IFF, NIFF, IF, but also subtype
+            if (iter.hasNext && iter.head == '<') {
+              consume()
+              tok(SUBTYPE, 2)
+            } else if (iter.hasNext && iter.head == '=') {
+              consume()
+              if (iter.hasNext && iter.head == '>') {
+                consume()
+                tok(IFF, 3)
+              } else {
+                tok(IF, 2)
+              }
+            } else if (iter.hasNext && iter.head == '~') {
+              consume()
+              if (iter.hasNext && iter.head == '>') {
+               consume()
+                tok(NIFF, 3)
+              } else {
+                throw new TPTPParseException("Unrecognized token '<~'", curLine, curOffset-2)
+              }
+            } else
+              throw new TPTPParseException("Unrecognized token '<'", curLine, curOffset-1)
+          case '=' => // IMPL or EQUALS
+            if (iter.hasNext && iter.head == '>') {
+              consume()
+              tok(IMPL, 2)
+            } else
+              tok(EQUALS, 1)
+          case '~' => // NOT, NAND, or NOR
+            if (iter.hasNext && iter.head == '&') {
+              consume()
+              tok(NAND, 2)
+            } else if (iter.hasNext && iter.head == '|') {
+              consume()
+              tok(NOR, 2)
+            } else
+              tok(NOT, 1)
           case '!' => ??? // FORALL, FORALLCOMB, TYFORAL, or NOTEQUALS
           case '?' => ??? // EXISTS, TYEXISTS, EXISTSCOMB
-          case '@' => ??? // CHOICE, DESC, COMBS of that and EQ, and APP  
+          case '@' => ??? // CHOICE, DESC, COMBS of that and EQ, and APP
           // remaining tokens
-          case '*' => STAR
-          case '+' => PLUS // TODO: Plus can also start a number
-          case '>' => RANGLE
-          case '.' => DOT
+          case _ if ch.isDigit && ch <= '9' => ??? // numbers
+          case '*' => tok(STAR, 1)
+          case '+' => // PLUS or number
+            if (iter.hasNext && isNumeric(iter.head)) {
+              ???
+            } else tok(PLUS, 1)
+          case '>' => tok(RANGLE, 1)
+          case '.' => tok(DOT, 1)
           case '\'' => ??? // single quoted
           case '"' => ??? // double quoted
           case '-' => ??? // Can start a number, or a sequent arrow
-          case '{' => LBRACES
-          case '}' => RBRACES
+          case '{' => tok(LBRACES, 1)
+          case '}' => tok(RBRACES, 1)
+          case _ => throw new TPTPParseException(s"Unrecognized token '$ch'", curLine, curOffset)
         }
-        ???
       }
     }
+    @inline private[this] def tok(tokType: TPTPLexer.TPTPLexerTokenType, length: Int): TPTPLexer.TPTPLexerToken =
+      (tokType, null, curLine, curOffset-length)
 
+    @inline private[this] def collectAlphaNums(startChar: Char): String = {
+      val sb: StringBuilder = new StringBuilder()
+      sb.append(startChar)
+      while (iter.hasNext && isAlphaNumeric(iter.head)) {
+        sb.append(consume())
+      }
+      sb.toString()
+    }
   }
   object TPTPLexer {
     type TPTPLexerToken = (TPTPLexerTokenType, Any, LineNo, Offset) // Cast Any to whatever it should be
