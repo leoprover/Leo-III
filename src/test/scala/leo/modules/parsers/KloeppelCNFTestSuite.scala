c6ce18eddeef25dc9f41bbfa63621a54b787a7be
Alexander Steen <a.steen@fu-berlin.de>
2020-12-13 18:45:25 +0100

    add some test cases for tptp-based test files for the parser

---
+++ src/test/scala/leo/modules/parsers/KloeppelCNFTestSuite.scala
@@ -0,0 +1,20 @@
+package leo.modules.parsers
+
+import leo.LeoTestSuite
+
+import java.io.File
+
+class KloeppelCNFTestSuite  extends LeoTestSuite {
+  val cnfFiles = new File("/home/lex/TPTP/Problems/SYN/").listFiles.filter(x => x.getName.endsWith(".p") && x.getName.contains("-")).toList
+
+  cnfFiles.foreach(f =>
+    test(f.getName) {
+      try {
+        val res = TPTPKloeppelParser.problem(io.Source.fromFile(f.getAbsolutePath))
+        println(res.pretty)
+      } catch {
+        case e: TPTPKloeppelParser.TPTPParseException => println(s"Parse error at ${e.line},${e.offset}: ${e.getMessage}"); throw e
+      }
+    }
+  )
+}
---
+++ src/test/scala/leo/modules/parsers/KloeppelTHFTestSuite.scala
@@ -0,0 +1,20 @@
+package leo.modules.parsers
+
+import leo.LeoTestSuite
+
+import java.io.File
+
+class KloeppelTHFTestSuite extends LeoTestSuite {
+  val th0Files = new File("/home/lex/TPTP/Problems/SYN/").listFiles.filter(x => x.getName.endsWith(".p") && x.getName.contains("^")).toList
+
+  th0Files.foreach(f =>
+    test(f.getName) {
+      try {
+        val res = TPTPKloeppelParser.problem(io.Source.fromFile(f.getAbsolutePath))
+        println(res.pretty)
+      } catch {
+        case e: TPTPKloeppelParser.TPTPParseException => println(s"Parse error at ${e.line},${e.offset}: ${e.getMessage}"); throw e
+      }
+    }
+  )
+}
