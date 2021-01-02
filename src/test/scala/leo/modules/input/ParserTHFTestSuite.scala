03d90ce0cc270c8683649d977c40c6b8a9f17b61
Alexander Steen <a.steen@fu-berlin.de>
2021-01-03 00:58:27 +0100

    minor parser test updates

--- src/test/scala/leo/modules/input/ParserTHFTestSuite.scala
+++ src/test/scala/leo/modules/input/ParserTHFTestSuite.scala
@@ -4,14 +4,13 @@ import leo.LeoTestSuite
 
 import java.io.File
 
-class KloeppelTHFTestSuite extends LeoTestSuite {
+class ParserTHFTestSuite extends LeoTestSuite {
   val th0Files = new File("/home/lex/TPTP/Problems/SYN/").listFiles.filter(x => x.getName.endsWith(".p") && x.getName.contains("^")).toList
 
   th0Files.foreach(f =>
     test(f.getName) {
       try {
-        val res = TPTPParser.problem(io.Source.fromFile(f.getAbsolutePath))
-        println(res.pretty)
+        TPTPParser.problem(io.Source.fromFile(f.getAbsolutePath))
       } catch {
         case e: TPTPParser.TPTPParseException => println(s"Parse error at ${e.line},${e.offset}: ${e.getMessage}"); throw e
       }
