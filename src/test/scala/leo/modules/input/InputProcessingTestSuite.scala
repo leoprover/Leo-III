457c7a31802a41cf7ee78c60ffe6a8970032fbd8
Alexander Steen <a.steen@fu-berlin.de>
2021-01-02 23:50:00 +0100

    updated inputprocessing test suite with TFF tests

--- src/test/scala/leo/modules/input/InputProcessingTestSuite.scala
+++ src/test/scala/leo/modules/input/InputProcessingTestSuite.scala
@@ -9,15 +9,15 @@ class InputProcessingTestSuite extends LeoTestSuite {
   private[this] val tptp = System.getenv("TPTP")
   private[this] val th0Files0 = new File(tptp ++ "/Problems/SYN/").listFiles.filter(x => x.getName.endsWith(".p") && x.getName.contains("^")).toList
   private[this] val th0Files = th0Files0.filterNot(_.getName == "SYN000^2.p")
+  private[this] val tffBasicFiles = Seq(new File(tptp ++ "/Problems/SYN/SYN000_1.p"), new File(tptp ++ "/Problems/SYN/SYN000_3.p"))
+  private[this] val tffFiles0 = new File(tptp ++ "/Problems/SYN/").listFiles.filter(x => x.getName.endsWith(".p") && x.getName.contains("_")).toList
+  private[this] val tffFiles = tffBasicFiles  ++ tffFiles0.filterNot(x => Seq("SYN000_1.p", "SYN000_3.p").contains(x.getName))
   private[this] val fofBasicFiles = Seq(new File(tptp ++ "/Problems/SYN/SYN000+1.p"), new File(tptp ++ "/Problems/SYN/SYN000+2.p"))
   private[this] val fofFiles0 = new File(tptp ++ "/Problems/SYN/").listFiles.filter(x => x.getName.endsWith(".p") && x.getName.contains("+")).toList
-  private[this] val fofFiles = fofBasicFiles // ++ fofFiles0
+  private[this] val fofFiles = fofBasicFiles //++ fofFiles0
   private[this] val cnfBasicFiles = Seq(new File(tptp ++ "/Problems/SYN/SYN000-1.p"), new File(tptp ++ "/Problems/SYN/SYN000-2.p"))
 
-//  val ltbFiles0 = new File("/home/lex/TPTP/CASC-2020/LTB/Problems/").listFiles.filter(x => x.getName.endsWith(".p") && x.getName.contains("^")).toList
-//  val ltbFiles = ltbFiles0.take(10)
-
-  private[this] val allFiles = fofFiles ++ cnfBasicFiles ++ th0Files  //++ ltbFiles
+  private[this] val allFiles = tffFiles ++ fofFiles ++ cnfBasicFiles ++ th0Files  //++ ltbFiles
 
   allFiles.foreach(f =>
     test(f.getName) {
